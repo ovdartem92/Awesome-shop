768628546c24479c2a03e8e3a1b85ec36316f7a9
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-10-16 10:31:13 +0300

    add method to Util

--- framework/src/main/java/util/Util.java
+++ framework/src/main/java/util/Util.java
@@ -1,4 +1,15 @@
 package util;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public interface Util {
+
+    static WebElement waitForElementLocatedBy(WebDriver driver, By by) {
+        return new WebDriverWait(driver, 25)
+                .until(ExpectedConditions.presenceOfElementLocated(by));
+    }
 }
