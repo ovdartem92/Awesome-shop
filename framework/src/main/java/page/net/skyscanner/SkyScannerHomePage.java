77f394b0a6ccaa2232e86f99eb171dc6e8495791
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-10-16 11:15:54 +0300

    create first test

--- framework/src/main/java/page/net/skyscanner/SkyScannerHomePage.java
+++ framework/src/main/java/page/net/skyscanner/SkyScannerHomePage.java
@@ -1,11 +1,26 @@
 package page.net.skyscanner;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import page.AbstractPage;
 
 public class SkyScannerHomePage extends AbstractPage {
 
-    protected SkyScannerHomePage(WebDriver driver) {
+    private static final String HOMEPAGE_URL = "https://www.skyscanner.net/";
+    private WebElement element;
+
+    public SkyScannerHomePage(WebDriver driver) {
         super(driver);
+    }
+
+    public SkyScannerHomePage openPage() {
+        driver.get(HOMEPAGE_URL);
+        new WebDriverWait(driver, 10);
+        return this;
+    }
+
+    public static String getHomepageUrl() {
+        return HOMEPAGE_URL;
     }
 }
--- framework/src/test/java/FirstTest.java
+++ framework/src/test/java/FirstTest.java
@@ -1,10 +1,17 @@
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import page.net.skyscanner.SkyScannerHomePage;
 
 public class FirstTest extends CommonConditions {
 
+    SoftAssert softAssert = new SoftAssert();
+
     @Test
     public void ourFirstTest() {
+        SkyScannerHomePage sky = new SkyScannerHomePage(driver)
+                .openPage();
 
+        softAssert.assertEquals(driver.getCurrentUrl(), sky.getHomepageUrl());
     }
 
 }
