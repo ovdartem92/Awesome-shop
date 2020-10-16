508e4b8a61a1028da84f616642a15568c30b80c2
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-10-16 14:09:06 +0300

    add: adding locators for hotel elements

--- framework/src/main/java/page/net/skyscanner/SkyScannerHomePage.java
+++ framework/src/main/java/page/net/skyscanner/SkyScannerHomePage.java
@@ -22,6 +22,30 @@ public class SkyScannerHomePage extends AbstractPage {
     private static final By closeButton = By.xpath("//button[@data-testid='modal-close-button']");
     private static final By wrongEmailOrPasswordTable = By.xpath("//span[text()='Wrong email or password']");
 
+    // Tab WebElements.
+    private static final By flightsTab = By.xpath("//nav[@id='PrimaryNav']//span[contains(text(), 'Flights')]");
+    private static final By hostelTab = By.xpath("//nav[@id='PrimaryNav']//span[contains(text(), 'Hotels')]");
+    private static final By carHireTab = By.xpath("//nav[@id='PrimaryNav']//span[contains(text(), 'Car Hire')]");
+
+    // Hotels WebElements.
+    private static final By destinationOrHostelNameInput = By.xpath("//input[@name='destination-autosuggest']");
+    private static final By hostelCheckInInput = By.xpath("//input[@id='checkin']");
+    private static final By hostelCheckOutInput = By.xpath("//input[@id='checkout']");
+    private static final By guestsAndRoomsInput = By.xpath("//input[@id='guests-rooms']");
+    private static final By searchButton = By.xpath("//button[contains(text(), 'Search hotels')]");
+
+    // Hotels guests and rooms WebElements.
+    private static final By quantityRoomsInput = By.xpath("//input[@id='rooms']");
+    private static final By increaseRoomButton = By.xpath("//button[@aria-controls='rooms'][@title='Increase']");
+    private static final By decreaseRoomButton = By.xpath("//button[@aria-controls='rooms'][@title='Decrease']");
+    private static final By quantityAdultPeopleInput = By.xpath("//input[@id='adults']");
+    private static final By increaseAdultButton = By.xpath("//button[@aria-controls='adults'][@title='Increase']");
+    private static final By decreaseAdultButton = By.xpath("//button[@aria-controls='adults'][@title='Decrease']");
+    private static final By quantityChildrenInput = By.xpath("//input[@id='children']");
+    private static final By increaseChildButton = By.xpath("//button[@aria-controls='children'][@title='Increase']");
+    private static final By decreaseChildButton = By.xpath("//button[@aria-controls='children'][@title='Decrease']");
+    private static final By doneButton = By.xpath("//footer/button");
+
     public SkyScannerHomePage(WebDriver driver) {
         super(driver);
     }
