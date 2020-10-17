6c90254da92b462c7ac3c97a2daa340757d20754
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-10-17 11:51:01 +0300

    create LogInWithCorrectData test

--- framework/src/test/java/LogInTest.java
+++ framework/src/test/java/LogInTest.java
@@ -1,4 +1,5 @@
 import model.User;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import page.net.skyscanner.SkyScannerHomePage;
@@ -6,14 +7,22 @@ import service.UserBuilder;
 
 public class LogInTest extends CommonConditions {
 
+    User user;
     SoftAssert softAssert = new SoftAssert();
-    User user = UserBuilder.getUserWithValidPassword();
-    User userInValid = UserBuilder.getUserWithInvalidPassword();
 
     @Test
     public void LogInWithCorrectData() {
         new SkyScannerHomePage(driver)
                 .openPage()
-                .logIn(user);
+                .logIn(user = UserBuilder.getUserWithValidPassword());
+        softAssert.assertEquals(true, driver.findElement(By.xpath("//button[@data-testid='btn-marketing-consent-cta']")));
+    }
+
+    @Test
+    public void LogInWithInCorrectData() {
+        new SkyScannerHomePage(driver)
+                .openPage()
+                .logIn(user = UserBuilder.getUserWithInvalidPassword());
+        softAssert.assertEquals(true, driver.findElement(By.xpath("//span[text()='Wrong email or password']")));
     }
 }
