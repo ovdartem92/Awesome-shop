3418334c033405b6aff545fa18d353aaa0a8e193
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-10-19 17:00:24 +0300

    add: adding test for checking sort of hotel's rating

---
+++ framework/src/test/java/CheckingSortOfRatingInHotelBookingTest.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+import page.net.skyscanner.SkyScannerHomePage;
+import page.net.skyscanner.hotel.SkyScannerHotelResultPage;
+import service.UserBuilder;
+
+public class CheckingSortOfRatingInHotelBookingTest extends CommonConditions {
+
+    @Test
+    public void checkingMaximumValues() {
+        String destination = "Turin";
+        user = UserBuilder.getUserWithValidPassword();
+
+        SkyScannerHotelResultPage resultPage  = new SkyScannerHomePage()
+                .openPage()
+                .switchToEnglish()
+                .logIn(user)
+                .clickToHostelsTab()
+                    .addDestination(destination)
+                    .clickToSearchHotelsButton()
+                        .clickToGuestRattingSortButton();
+
+        softAssert.assertEquals(resultPage.isHotelsSortedByRating(), true);
+    }
+}
