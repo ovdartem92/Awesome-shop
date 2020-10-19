9ff41414eed107c76294fd0b66d0ceb0d2a2ced5
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-10-19 14:50:45 +0300

    fix: adding changes in hotel result and search pages

--- framework/src/main/java/page/net/skyscanner/hotel/SkyScannerSearchHotelPage.java
+++ framework/src/main/java/page/net/skyscanner/hotel/SkyScannerSearchHotelPage.java
@@ -2,6 +2,9 @@ package page.net.skyscanner.hotel;
 
 import org.openqa.selenium.By;
 import page.AbstractPage;
+import page.net.skyscanner.SkyScannerCarSearchPage;
+
+import static service.ActionManager.*;
 
 public class SkyScannerSearchHotelPage extends AbstractPage {
 
@@ -23,86 +26,84 @@ public class SkyScannerSearchHotelPage extends AbstractPage {
     private static final By SEARCH_HOTELS_BUTTON = By.xpath("//form[@id='search-controls']//button");
     private static final By CAR_HIRE_TAB = By.xpath("//a[@id='carhi']");
 
-//    public SkyScannerSearchHotelPage addDestination(String destination) {
-//        waitForElementLocatedBy(driver, DESTINATION_OR_HOSTEL_NAME_INPUT).click();
-//        waitForElementLocatedBy(driver, DESTINATION_OR_HOSTEL_NAME_INPUT).sendKeys(destination);
-//        return this;
-//    }
-//
-//    public SkyScannerSearchHotelPage increaseRoom() {
-//        waitForElementLocatedBy(driver, GUESTS_AND_ROOM_INPUT).click();
-//        waitForElementLocatedBy(driver, INCREASE_ROOM_BUTTON).click();
-//        waitForElementLocatedBy(driver, DONE_BUTTON).click();
-//        return this;
-//    }
-//
-//    public SkyScannerCarSearchPage clickToCarHireTab() {
-//        waitForElementLocatedBy(driver, CAR_HIRE_TAB);
-//        driver.findElement(CAR_HIRE_TAB).click();
-//        LOGGER.info(("Clicked on CAR_HIRE_TAB"));
-//        return new SkyScannerCarSearchPage(driver);
-//    }
-//
-//    public SkyScannerSearchHotelPage increaseRoom(int quantity) {
-//        for (int i = 0; i < quantity; i++)
-//            increaseRoom();
-//        return this;
-//    }
-//
-//    public SkyScannerSearchHotelPage increaseAdult() {
-//        waitForElementLocatedBy(driver, GUESTS_AND_ROOM_INPUT).click();
-//        waitForElementLocatedBy(driver, INCREASE_ADULT_BUTTON).click();
-//        waitForElementLocatedBy(driver, DONE_BUTTON).click();
-//        return this;
-//    }
-//
-//    public SkyScannerSearchHotelPage increaseAdult(int quantity) {
-//        for (int i = 0; i < quantity; i++)
-//            increaseAdult();
-//        return this;
-//    }
-//
-//    public SkyScannerSearchHotelPage increaseChild() {
-//        waitForElementLocatedBy(driver, GUESTS_AND_ROOM_INPUT).click();
-//        waitForElementLocatedBy(driver, INCREASE_CHILD_BUTTON).click();
-//        waitForElementLocatedBy(driver, DONE_BUTTON).click();
-//        return this;
-//    }
-//
-//    public SkyScannerSearchHotelPage increaseChild(int quantity) {
-//        for (int i = 0; i < quantity; i++)
-//            increaseChild();
-//        return this;
-//    }
-//
-//    public String getQuantityRooms() {
-//        waitForElementLocatedBy(driver, GUESTS_AND_ROOM_INPUT).click();
-//        String quantity = waitForElementLocatedBy(driver, QUANTITY_ROOMS_INPUT).getAttribute("value");
-//        waitForElementLocatedBy(driver, DONE_BUTTON).click();
-//        return quantity;
-//    }
-//
-//    public String getQuantityAdultPeople() {
-//        waitForElementLocatedBy(driver, GUESTS_AND_ROOM_INPUT).click();
-//        String quantity = waitForElementLocatedBy(driver, QUANTITY_ADULT_PEOPLE_INPUT).getAttribute("value");
-//        waitForElementLocatedBy(driver, DONE_BUTTON).click();
-//        return quantity;
-//    }
-//
-//    public String getQuantityChildren() {
-//        waitForElementLocatedBy(driver, GUESTS_AND_ROOM_INPUT).click();
-//        String quantity = waitForElementLocatedBy(driver, QUANTITY_CHILDREN_INPUT).getAttribute("value");
-//        waitForElementLocatedBy(driver, DONE_BUTTON).click();
-//        return quantity;
-//    }
-//
-//    public SkyScannerHotelResultPage clickToSearchHotelsButton() {
-//        waitForElementLocatedBy(driver, SEARCH_HOTELS_BUTTON).click();
-//        return new SkyScannerHotelResultPage(driver);
-//    }
-//
-//    public String getTextFromHotelButton() {
-//        return waitForElementLocatedBy(driver, SEARCH_HOTEL_BUTTON).getText();
-//    }
+    public SkyScannerSearchHotelPage addDestination(String destination) {
+        clickOnElementBy(DESTINATION_OR_HOSTEL_NAME_INPUT);
+        typeTextToElementBy(DESTINATION_OR_HOSTEL_NAME_INPUT, destination);
+        return this;
+    }
 
+    public SkyScannerSearchHotelPage increaseRoom() {
+        clickOnElementBy(GUESTS_AND_ROOM_INPUT);
+        clickOnElementBy(INCREASE_ROOM_BUTTON);
+        clickOnElementBy(DONE_BUTTON);
+        return this;
+    }
+
+    public SkyScannerCarSearchPage clickToCarHireTab() {
+        clickOnElementBy(CAR_HIRE_TAB);
+        logger.info(("Clicked on CAR_HIRE_TAB"));
+        return new SkyScannerCarSearchPage();
+    }
+
+    public SkyScannerSearchHotelPage increaseRoom(int quantity) {
+        for (int i = 0; i < quantity; i++)
+            increaseRoom();
+        return this;
+    }
+
+    public SkyScannerSearchHotelPage increaseAdult() {
+        clickOnElementBy(GUESTS_AND_ROOM_INPUT);
+        clickOnElementBy(INCREASE_ADULT_BUTTON);
+        clickOnElementBy(DONE_BUTTON);
+        return this;
+    }
+
+    public SkyScannerSearchHotelPage increaseAdult(int quantity) {
+        for (int i = 0; i < quantity; i++)
+            increaseAdult();
+        return this;
+    }
+
+    public SkyScannerSearchHotelPage increaseChild() {
+        clickOnElementBy(GUESTS_AND_ROOM_INPUT);
+        clickOnElementBy(INCREASE_CHILD_BUTTON);
+        clickOnElementBy(DONE_BUTTON);
+        return this;
+    }
+
+    public SkyScannerSearchHotelPage increaseChild(int quantity) {
+        for (int i = 0; i < quantity; i++)
+            increaseChild();
+        return this;
+    }
+
+    public String getQuantityRooms() {
+        clickOnElementBy(GUESTS_AND_ROOM_INPUT);
+        String quantity = getAttributeValueOnElementBy(QUANTITY_ROOMS_INPUT,"value");
+        clickOnElementBy(DONE_BUTTON);
+        return quantity;
+    }
+
+    public String getQuantityAdultPeople() {
+        clickOnElementBy(GUESTS_AND_ROOM_INPUT);
+        String quantity = getAttributeValueOnElementBy(QUANTITY_ADULT_PEOPLE_INPUT,"value");
+        clickOnElementBy(DONE_BUTTON);
+        return quantity;
+    }
+
+    public String getQuantityChildren() {
+        clickOnElementBy(GUESTS_AND_ROOM_INPUT);
+        String quantity = getAttributeValueOnElementBy(QUANTITY_CHILDREN_INPUT,"value");
+        clickOnElementBy(DONE_BUTTON);
+        return quantity;
+    }
+
+    public SkyScannerHotelResultPage clickToSearchHotelsButton() {
+        clickOnElementBy(SEARCH_HOTELS_BUTTON);
+        return new SkyScannerHotelResultPage();
+    }
+
+    public String getTextFromHotelButton() {
+        return getTextOnElementBy(SEARCH_HOTEL_BUTTON);
+    }
 }
