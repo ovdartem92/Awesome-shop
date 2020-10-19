7e759bdaf4b43064c82a4ce7fbc9445c1dad6871
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-10-19 14:55:48 +0300

    add _LOCATOR to locator's name

--- framework/src/main/java/page/net/skyscanner/SkyScannerProfilePage.java
+++ framework/src/main/java/page/net/skyscanner/SkyScannerProfilePage.java
@@ -7,10 +7,10 @@ import static service.ActionManager.clickOnElementBy;
 
 public class SkyScannerProfilePage extends AbstractPage {
 
-    private static final By ACCOUNT_FIELD = By.xpath("//span[@class='BpkText_bpk-text__Pt0NU BpkText_bpk-text--xs__2OIqg Menu_MenuItem__subtitle__2dCg3']");
+    private static final By ACCOUNT_FIELD_LOCATOR = By.xpath("//span[@class='BpkText_bpk-text__Pt0NU BpkText_bpk-text--xs__2OIqg Menu_MenuItem__subtitle__2dCg3']");
 
     public SkyScannerProfilePage clickOnAccountField() {
-        clickOnElementBy(ACCOUNT_FIELD);
+        clickOnElementBy(ACCOUNT_FIELD_LOCATOR);
         return this;
     }
 }
