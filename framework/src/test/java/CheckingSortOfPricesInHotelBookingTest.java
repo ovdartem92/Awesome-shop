0d2265b49664ddfe34e19904aa49f16a65d8f3c7
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-10-19 14:51:12 +0300

    fix: adding changes in hotels tests

--- framework/src/test/java/CheckingSortOfPricesInHotelBookingTest.java
+++ framework/src/test/java/CheckingSortOfPricesInHotelBookingTest.java
@@ -1,24 +1,26 @@
 import org.testng.annotations.Test;
 import page.net.skyscanner.SkyScannerHomePage;
+import page.net.skyscanner.hotel.SkyScannerHotelResultPage;
+import page.net.skyscanner.hotel.SkyScannerSearchHotelPage;
 import service.UserBuilder;
 
 public class CheckingSortOfPricesInHotelBookingTest extends CommonConditions {
 
-//    @Test
-//    public void checkingMaximumValues() {
-//        String destination = "Turin";
-//        user = UserBuilder.getUserWithValidPassword();
-//
-//        SkyScannerSearchHotelPage searchHotelPage =
-//                new SkyScannerHomePage(driver)
-//                .openPage()
-//                .logIn(user)
-//                .clickToHostelsTab();
-//
-//        SkyScannerHotelResultPage resultPage = searchHotelPage.addDestination(destination)
-//                                                              .clickToSearchHotelsButton()
-//                                                              .clickToPriceSortButton();
-//
-//        softAssert.assertEquals(resultPage.isHotelsSortedByPrice(), true);
-//    }
+    @Test
+    public void checkingMaximumValues() {
+        String destination = "Turin";
+        user = UserBuilder.getUserWithValidPassword();
+
+        SkyScannerSearchHotelPage searchHotelPage =
+                new SkyScannerHomePage()
+                .openPage()
+                .logIn(user)
+                .clickToHostelsTab();
+
+        SkyScannerHotelResultPage resultPage = searchHotelPage.addDestination(destination)
+                                                              .clickToSearchHotelsButton()
+                                                              .clickToPriceSortButton();
+
+        softAssert.assertEquals(resultPage.isHotelsSortedByPrice(), true);
+    }
 }
