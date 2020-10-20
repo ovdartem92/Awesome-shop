e79d2568860cc28fa2b99c690bbc39f2421c7735
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-10-20 14:17:33 +0300

    add: adding enum DefaultTimeoutValues

---
+++ framework/src/main/java/enums/DefaultTimeoutValues.java
@@ -0,0 +1,15 @@
+package enums;
+
+public enum DefaultTimeoutValues {
+    SHORT_TIMEOUT_MILLIS(500), LONG_TIMEOUT_SECONDS(15), DEFAULT_TIMEOUT_SECONDS(5);
+
+    private int value;
+
+    DefaultTimeoutValues(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
