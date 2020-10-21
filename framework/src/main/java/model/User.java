59a176e81a750c89339f0f56cb7e34c99e3b13c8
Anastasiya Murashka <anastasiya_murashka@epam.com>
2020-10-21 16:07:35 +0300

    update models

--- framework/src/main/java/model/User.java
+++ framework/src/main/java/model/User.java
@@ -2,12 +2,10 @@ package model;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
-@Builder(toBuilder = true)
 public class User {
 
     private String email;
