41ea1d3317a115d5229c162d0345b8e16af4b6e2
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-10-22 19:27:31 +0300

    add some fix and change

--- framework/src/test/java/CheckYourEmailAfterLogInTest.java
+++ framework/src/test/java/CheckYourEmailAfterLogInTest.java
@@ -4,8 +4,6 @@ import page.net.skyscanner.SkyScannerHomePage;
 import page.net.skyscanner.SkyScannerProfilePage;
 import service.UserBuilder;
 
-import static service.ActionManager.getElementBy;
-
 public class CheckYourEmailAfterLogInTest extends BaseTest {
     SoftAssert softAssert = new SoftAssert();
 
