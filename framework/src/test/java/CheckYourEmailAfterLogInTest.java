c17ebe06370b046870421f19b1340ef6bfb3dc56
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-10-23 14:28:14 +0300

    refactor: change softAssert to Assert in CheckYourEmailAfterLogInTest.java

--- framework/src/test/java/CheckYourEmailAfterLogInTest.java
+++ framework/src/test/java/CheckYourEmailAfterLogInTest.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import page.net.skyscanner.SkyScannerHomePage;
@@ -8,12 +9,11 @@ public class CheckYourEmailAfterLogInTest extends BaseTest {
 
     @Test
     public void checkYourEmail() {
-        SoftAssert softAssert = new SoftAssert();
         new SkyScannerHomePage()
                 .logIn(user = UserBuilder.getUserWithValidPassword())
                 .openProfilePage()
                 .switchToEnglish()
                 .clickOnAccountField();
-        softAssert.assertEquals(user.getEmail(), new SkyScannerProfilePage().findAndGetUserEmail(user));
+        Assert.assertTrue(new SkyScannerProfilePage().findAndGetUserEmail(user));
     }
 }
