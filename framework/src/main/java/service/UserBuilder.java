0fa2947a13c62f68dd037daef43163e5e9263b41
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-10-23 11:11:27 +0300

    feat: add default currency to UserBuilder

--- framework/src/main/java/service/UserBuilder.java
+++ framework/src/main/java/service/UserBuilder.java
@@ -5,8 +5,8 @@ import model.User;
 import static util.RandomString.getRandomString;
 
 public class UserBuilder {
-    private static final String EMAIL = getEmailToProperties();
-    private static final String PASSWORD = getPasswordToProperties();
+    private static final String EMAIL = TestDataReader.getTestData("testData.email");
+    private static final String PASSWORD = TestDataReader.getTestData("testData.password");
     private static final String INVALID_PASSWORD = getRandomString();
 
     public static User getUserWithValidPassword() {
@@ -15,19 +15,5 @@ public class UserBuilder {
 
     public static User getUserWithInvalidPassword() {
         return new User(EMAIL, INVALID_PASSWORD);
-    }
-
-    public static String getEmailToProperties() {
-        if (System.getProperty("testData.email") == null) {
-            return "user@mail.com";
-        }
-        return TestDataReader.getTestData("testData.email");
-    }
-
-    public static String getPasswordToProperties() {
-        if (System.getProperty("testData.password") == null) {
-            return getRandomString();
-        }
-        return TestDataReader.getTestData("testData.password");
     }
 }
