c69759fc375da30ac311ccb70c5cf69b24b74d2d
Anastasiya Murashka <anastasiya_murashka@epam.com>
2020-10-24 13:31:47 +0300

    add method checkCaptcha to ActionManager

--- framework/src/main/java/service/ActionManager.java
+++ framework/src/main/java/service/ActionManager.java
@@ -1,6 +1,8 @@
 package service;
 
 import driver.Browser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -8,9 +10,13 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import static util.CaptchaMethod.checkCaptchaOnPage;
+
 public interface ActionManager {
+    Logger logger = LogManager.getRootLogger();
 
     static WebElement getElementBy(String locatorPath) {
+        checkCaptchaOnPage(logger);
         if (!WaitManager.isElementVisibleBy(locatorPath))
             throw new NoSuchElementException(String.format("There is no element with a locator %s on the page.", locatorPath));
         return Browser.getDriver().findElement(By.xpath(locatorPath));
