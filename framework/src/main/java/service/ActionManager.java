b09737add1838ed964603dfbd302ad6e62e520b5
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-10-24 10:52:37 +0300

    fix: changing method getElementBy on ActionManager

--- framework/src/main/java/service/ActionManager.java
+++ framework/src/main/java/service/ActionManager.java
@@ -11,6 +11,8 @@ import java.util.NoSuchElementException;
 public interface ActionManager {
 
     static WebElement getElementBy(String locatorPath) {
+        if (!WaitManager.isElementVisibleBy(locatorPath))
+            throw new NoSuchElementException(String.format("There is no element with a locator %s on the page.", locatorPath));
         return Browser.getDriver().findElement(By.xpath(locatorPath));
     }
 
@@ -24,14 +26,10 @@ public interface ActionManager {
     }
 
     static String getTextOnElementBy(String locatorPath) {
-        if (!WaitManager.isElementVisibleBy(locatorPath))
-            throw new NoSuchElementException(String.format("There is no element with a locator %s on the page.", locatorPath));
         return getElementBy(locatorPath).getText();
     }
 
     static String getAttributeValueOnElementBy(String locatorPath, String attribute) {
-        if (!WaitManager.isElementVisibleBy(locatorPath))
-            throw new NoSuchElementException(String.format("There is no element with a locator %s on the page.", locatorPath));
         return getElementBy(locatorPath).getAttribute(attribute);
     }
 
