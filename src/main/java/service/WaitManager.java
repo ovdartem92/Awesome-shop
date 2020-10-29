7918fd529201824f07c9a765a1debedcbc2ac495
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-10-29 11:31:07 +0300

    feat: add method isElementDisplayed in WaitManager class

--- src/main/java/service/WaitManager.java
+++ src/main/java/service/WaitManager.java
@@ -19,7 +19,7 @@ public abstract class WaitManager {
                 .ignoring(NoSuchElementException.class, StaleElementReferenceException.class);
     }
 
-    public static boolean isElementVisibleBy(String locatorPath) {
+    public static boolean isElementVisible(String locatorPath) {
         Wait<WebDriver> wait = getDefaultWaitConfig();
         try {
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locatorPath)));
@@ -29,17 +29,22 @@ public abstract class WaitManager {
         }
     }
 
-    public static WebElement waitForElementLocatedBy(String locatorPath) {
+    public static boolean isElementDisplayed(String locatorPath) {
+        return new WebDriverWait(Browser.initDriver(), Browser.SHORT_TIMEOUT_SECONDS).until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath(locatorPath))).isDisplayed();
+    }
+
+    public static WebElement waitForElementLocated(String locatorPath) {
         Wait<WebDriver> wait = getDefaultWaitConfig();
         return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locatorPath)));
     }
 
-    public static List<WebElement> waitForAllElementsLocatedBy(String locatorPath) {
+    public static List<WebElement> waitForAllElementsLocated(String locatorPath) {
         Wait<WebDriver> wait = getDefaultWaitConfig();
         return wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(locatorPath)));
     }
 
-    public static WebElement waitForElementToBeClickableBy(String locatorPath) {
+    public static WebElement waitForElementToBeClickable(String locatorPath) {
         Wait<WebDriver> wait = getDefaultWaitConfig();
         return wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locatorPath)));
     }
