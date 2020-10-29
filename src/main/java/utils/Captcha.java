cc3766e4ad78c3eb26c8649961974981327197f2
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-10-29 11:34:36 +0300

    refactor: change class name CaptchaMethod to Captcha

--- src/main/java/utils/Captcha.java
+++ src/main/java/utils/Captcha.java
@@ -2,18 +2,15 @@ package utils;
 
 import driver.Browser;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.SkipException;
 
+import static service.WaitManager.isElementDisplayed;
 
-public final class CaptchaMethod {
+public final class Captcha {
     private static final String CAPTCHA_ELEMENT_LOCATOR = "//img[contains(@class,'BpkImage')]";
 
     public static boolean checkCaptchaOnPage(Logger logger) {
-        if (new WebDriverWait(Browser.initDriver(), Browser.SHORT_TIMEOUT_SECONDS).until(
-                ExpectedConditions.presenceOfElementLocated(By.xpath(CAPTCHA_ELEMENT_LOCATOR))).isDisplayed()) {
+        if (isElementDisplayed(CAPTCHA_ELEMENT_LOCATOR)) {
             logger.info("Captcha element is present on pages.page: {}", Browser.initDriver().getCurrentUrl());
             throw new SkipException("Test was skipped, because CAPTCHA has appeared");
         }
