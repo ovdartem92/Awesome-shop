5182981614ccc48e6c35dae85cdbc1ce64733930
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-10-30 16:53:57 +0300

    refactor: rename final SecureRandom random to RANDOM

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -8,7 +8,7 @@ public abstract class StringUtils {
     private final static String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz1234567890";
     private final static String CHAR_UPPER = CHAR_LOWER.toUpperCase();
     private final static String DATA_FOR_NEW_STRING = CHAR_LOWER + CHAR_UPPER;
-    private final static SecureRandom random = new SecureRandom();
+    private final static SecureRandom RANDOM = new SecureRandom();
 
     public static String getCurrentTimeAsString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd_HH-mm-ss");
@@ -18,7 +18,7 @@ public abstract class StringUtils {
     public static String getRandomString() {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < 8; i++) {
-            int randomIndex = random.nextInt(DATA_FOR_NEW_STRING.length());
+            int randomIndex = RANDOM.nextInt(DATA_FOR_NEW_STRING.length());
             char randomChar = DATA_FOR_NEW_STRING.charAt(randomIndex);
             stringBuilder.append(randomChar);
         }
