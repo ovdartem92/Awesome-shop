274ff00209602abc4ba6ed09b2d3c4dded265010
Anastasiya Murashka <anastasiya_murashka@epam.com>
2020-11-05 11:29:16 +0300

    refactor header

--- src/main/java/pages/net/skyscanner/elements/HeaderScreen.java
+++ src/main/java/pages/net/skyscanner/elements/HeaderScreen.java
@@ -8,7 +8,7 @@ public class HeaderScreen extends AbstractScreen {
     private static final String HOTELS_TAB_TEXT = "Hotels";
     private static final String CAR_TAB_TEXT = "Car";
     private static final String HEADER_TAB_LOCATOR = ("//nav[@id='PrimaryNav']//span[contains(text(),'%s')]");
-    private static final String CAR_HIRE_TAB_BUTTON_LOCATOR =String.format(HEADER_TAB_LOCATOR, CAR_TAB_TEXT);
+    private static final String CAR_HIRE_TAB_BUTTON_LOCATOR = String.format(HEADER_TAB_LOCATOR, CAR_TAB_TEXT);
     private static final String FLIGHTS_TAB_BUTTON_LOCATOR = String.format(HEADER_TAB_LOCATOR, FLIGHTS_TAB_TEXT);
     private static final String HOTEL_TAB_BUTTON_LOCATOR = String.format(HEADER_TAB_LOCATOR, HOTELS_TAB_TEXT);
     private static final String CULTURE_SETTING_BUTTON_LOCATOR = "//li[@id='culture-info']//button";
