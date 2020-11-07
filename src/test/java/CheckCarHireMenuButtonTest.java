dfb5aef55dbef5b99dc423aea94bf34c5ddbeeac
Anastasiya Murashka <anastasiya_murashka@epam.com>
2020-11-07 09:52:52 +0300

    refactor check menu button tests

--- src/test/java/CheckCarHireMenuButtonTest.java
+++ src/test/java/CheckCarHireMenuButtonTest.java
@@ -4,11 +4,12 @@ import pages.net.skyscanner.flights.FlightsSearchScreen;
 import utils.Constants;
 
 public class CheckCarHireMenuButtonTest extends BaseTest {
+    private final String TEXT_ON_CAR_SEARCH_PAGE = "Find your ride";
 
     @Test(description = "check that car hire menu button leads to the car search page")
     public void checkCarHireMenuButton() {
         FlightsSearchScreen.changeLanguage(Constants.ENGLISH_LANGUAGE);
-        Assert.assertEquals(FlightsSearchScreen.header.clickCarButton().getTextFromCarHeadline(), "Find your ride",
+        Assert.assertEquals(FlightsSearchScreen.header.clickCarButton().getTextFromCarHeadline(), TEXT_ON_CAR_SEARCH_PAGE,
                 "car hire menu button doesn't lead to the car search page");
     }
 }
--- src/test/java/CheckFlightsMenuButtonTest.java
+++ src/test/java/CheckFlightsMenuButtonTest.java
@@ -4,12 +4,13 @@ import pages.net.skyscanner.flights.FlightsSearchScreen;
 import utils.Constants;
 
 public class CheckFlightsMenuButtonTest extends BaseTest {
+    private final String TEXT_ON_FLIGHTS_SEARCH_PAGE = "Search flights";
 
     @Test(description = "check that flights menu button leads to the flights search page")
     public void checkFlightsMenuButton() {
         FlightsSearchScreen.changeLanguage(Constants.ENGLISH_LANGUAGE);
         Assert.assertEquals(FlightsSearchScreen.header.clickFlightButton()
-                        .getTextFromFlightsSearchButton(), "Search flights",
+                        .getTextFromFlightsSearchButton(), TEXT_ON_FLIGHTS_SEARCH_PAGE,
                 "flights menu button doesn't lead to the flights search page");
     }
 }
