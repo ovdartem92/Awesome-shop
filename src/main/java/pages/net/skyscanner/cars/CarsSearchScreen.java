5572be37b121974f8bfa92e68f62971cfcc0530b
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-08 12:06:09 +0300

    fix: create variables for locations and fix typing of input fields

--- src/main/java/pages/net/skyscanner/cars/CarsSearchScreen.java
+++ src/main/java/pages/net/skyscanner/cars/CarsSearchScreen.java
@@ -9,6 +9,7 @@ public class CarsSearchScreen extends AbstractScreen {
     private static final String PICKUP_LOCATION_LOCATOR = "//input[@id='carhire-search-controls-location-pick-up']";
     private static final String DROP_OFF_LOCATION_LOCATOR = "//input[@id='carhire-search-controls-location-drop-off']";
     private static final String RETURN_CAR_TO_A_DIFFERENT_LOCATION_CHECKBOX_LOCATOR = "//input[@id='carhire-search-controls-different-drop-off-docked']";
+    private static final String FIRST_ELEMENT_OF_DROPDOWN_LOCATOR = "//input[@aria-activedescendant='react-autowhatever-1--item-0']";
 
     public String getTextFromCarHeadline() {
         return getTextOnElement(CAR_HEADLINE_LOCATOR);
@@ -16,7 +17,8 @@ public class CarsSearchScreen extends AbstractScreen {
 
     public CarsSearchScreen setUpPickUpLocation(String pickUpLocation) {
         clickOnElement(PICKUP_LOCATION_LOCATOR);
-        typeInFieldWithDelay(PICKUP_LOCATION_LOCATOR, pickUpLocation);
+        typeTextToElement(PICKUP_LOCATION_LOCATOR, pickUpLocation);
+        clickOnElement(FIRST_ELEMENT_OF_DROPDOWN_LOCATOR);
         return this;
     }
 
@@ -27,7 +29,8 @@ public class CarsSearchScreen extends AbstractScreen {
 
     public CarsSearchScreen setUpDropOffLocation(String dropOffLocationLocation) {
         clickOnElement(DROP_OFF_LOCATION_LOCATOR);
-        typeInFieldWithDelay(DROP_OFF_LOCATION_LOCATOR, dropOffLocationLocation);
+        typeTextToElement(DROP_OFF_LOCATION_LOCATOR, dropOffLocationLocation);
+        clickOnElement(FIRST_ELEMENT_OF_DROPDOWN_LOCATOR);
         return this;
     }
 
--- src/test/java/CheckPickUpAndDropOffLocationsSummaryTest.java
+++ src/test/java/CheckPickUpAndDropOffLocationsSummaryTest.java
@@ -7,18 +7,19 @@ import utils.Constants;
 public class CheckPickUpAndDropOffLocationsSummaryTest extends BaseTest {
     @Test
     public void checkingPickupAndDropOffLocations() {
-
+        String pickUpLocation = Constants.MOSCOW_SHEREMETYEVO_SVO;
+        String dropOffLocation = Constants.MOSCOW_VNUKOVO_VKO;
         HeaderScreen.changeLanguage(Constants.ENGLISH_LANGUAGE);
         CarsSearchResultsScreen carsSearchResultsScreen = new HeaderScreen()
                 .clickCarButton()
                 .choiceReturnCarToADifferentLocation()
-                .setUpPickUpLocation(Constants.MOSCOW_SHEREMETYEVO_SVO)
-                .setUpDropOffLocation(Constants.MOSCOW_VNUKOVO_VKO)
+                .setUpPickUpLocation(pickUpLocation)
+                .setUpDropOffLocation(dropOffLocation)
                 .clickSearchButton();
 
-        Assert.assertEquals(Constants.MOSCOW_SHEREMETYEVO_SVO, carsSearchResultsScreen.getInfoAboutPickUpLocationFromSummary(),
+        Assert.assertEquals(pickUpLocation, carsSearchResultsScreen.getInfoAboutPickUpLocationFromSummary(),
                 "Input set up location are not equals summary set up location.");
-        Assert.assertEquals(Constants.MOSCOW_VNUKOVO_VKO, carsSearchResultsScreen.getInfoAboutDropOffLocationFromSummary(),
+        Assert.assertEquals(dropOffLocation, carsSearchResultsScreen.getInfoAboutDropOffLocationFromSummary(),
                 "Input drop off location are not equals summary drop off location.");
 
     }
