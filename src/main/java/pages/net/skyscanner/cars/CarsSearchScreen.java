3658dc694c2e2b53f912ac4995993edfd01a7207
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-12 22:07:53 +0300

    fix: make dropdpwnn locator protected

--- src/main/java/pages/net/skyscanner/cars/CarsSearchScreen.java
+++ src/main/java/pages/net/skyscanner/cars/CarsSearchScreen.java
@@ -9,7 +9,6 @@ public class CarsSearchScreen extends AbstractScreen {
     private static final String PICKUP_LOCATION_LOCATOR = "//input[@id='carhire-search-controls-location-pick-up']";
     private static final String DROP_OFF_LOCATION_LOCATOR = "//input[@id='carhire-search-controls-location-drop-off']";
     private static final String RETURN_CAR_TO_A_DIFFERENT_LOCATION_CHECKBOX_LOCATOR = "//input[@id='carhire-search-controls-different-drop-off-docked']";
-    private static final String FIRST_ELEMENT_OF_DROPDOWN_LOCATOR = "//input[@aria-activedescendant='react-autowhatever-1--item-0']";
 
     public String getTextFromCarHeadline() {
         return getTextOnElement(CAR_HEADLINE_LOCATOR);
