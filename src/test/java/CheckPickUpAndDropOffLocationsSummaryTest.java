88ca6716689e9478b157c3d61520f9c1ea6c3cdc
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-12 11:00:09 +0300

    ref: fix CheckPickUpAndDropOffLocationsSummaryTest

--- src/test/java/CheckPickUpAndDropOffLocationsSummaryTest.java
+++ src/test/java/CheckPickUpAndDropOffLocationsSummaryTest.java
@@ -1,12 +1,14 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.net.skyscanner.cars.CarsSearchResultsScreen;
 import pages.net.skyscanner.elements.HeaderScreen;
 import utils.Constants;
 
 public class CheckPickUpAndDropOffLocationsSummaryTest extends BaseTest {
-    @Test
+    @Test(description = "check input values of locations are equals locations in search result page summary")
     public void checkingPickupAndDropOffLocations() {
+        SoftAssert softAssert = new SoftAssert();
         String pickUpLocation = Constants.MOSCOW_SHEREMETYEVO_SVO;
         String dropOffLocation = Constants.MOSCOW_VNUKOVO_VKO;
         HeaderScreen.changeLanguage(Constants.ENGLISH_LANGUAGE);
@@ -17,10 +19,13 @@ public class CheckPickUpAndDropOffLocationsSummaryTest extends BaseTest {
                 .setUpDropOffLocation(dropOffLocation)
                 .clickSearchButton();
 
-        Assert.assertEquals(pickUpLocation, carsSearchResultsScreen.getInfoAboutPickUpLocationFromSummary(),
-                "Input set up location is not equals summary set up location.");
-        Assert.assertEquals(dropOffLocation, carsSearchResultsScreen.getInfoAboutDropOffLocationFromSummary(),
-                "Input drop off location is not equals summary drop off location.");
+        String pickUpLocationSummary = carsSearchResultsScreen.getInfoAboutPickUpLocationFromSummary();
+        String dropOffLocationSummary = carsSearchResultsScreen.getInfoAboutDropOffLocationFromSummary();
 
+        softAssert.assertEquals(pickUpLocation, pickUpLocationSummary,
+                "Input set up location is not equals summary set up location.");
+        softAssert.assertEquals(dropOffLocation, dropOffLocationSummary,
+                "Input drop off location is not equals summary drop off location.");
+        softAssert.assertAll();
     }
 }
