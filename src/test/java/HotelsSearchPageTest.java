2c87d5deda40c0a5a7c3b9643d0bdffeda65fb09
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-12 19:30:08 +0300

    refactor: change checkMaximumValuesOfFilterButtons test

--- src/test/java/HotelsSearchPageTest.java
+++ src/test/java/HotelsSearchPageTest.java
@@ -2,7 +2,8 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.net.skyscanner.elements.HeaderScreen;
 import pages.net.skyscanner.hotels.HotelsSearchScreen;
-import pages.net.skyscanner.hotels.source.FilterButtonsScreen;
+import pages.net.skyscanner.hotels.source.FilterButtonsScreen.Title;
+import pages.net.skyscanner.hotels.source.FilterButtonsScreen.Id;
 
 public class HotelsSearchPageTest extends BaseTest {
     private final int MAX_ROOMS_QUANTITY = 5;
@@ -16,13 +17,13 @@ public class HotelsSearchPageTest extends BaseTest {
         int adultQuantityClicks = 5;
 
         HotelsSearchScreen searchHotelScreen = new HeaderScreen().clickHotelButton();
-        searchHotelScreen.filterButtons.clickButton(FilterButtonsScreen.Title.INCREASE, FilterButtonsScreen.Id.ROOMS, roomsQuantityClicks);
-        searchHotelScreen.filterButtons.clickButton(FilterButtonsScreen.Title.INCREASE, FilterButtonsScreen.Id.ADULTS, adultQuantityClicks);
-        searchHotelScreen.filterButtons.clickButton(FilterButtonsScreen.Title.INCREASE, FilterButtonsScreen.Id.CHILDREN, childrenQuantityClicks);
+        searchHotelScreen.filterButtons.clickButton(Title.INCREASE, Id.ROOMS, roomsQuantityClicks);
+        searchHotelScreen.filterButtons.clickButton(Title.INCREASE, Id.ADULTS, adultQuantityClicks);
+        searchHotelScreen.filterButtons.clickButton(Title.INCREASE, Id.CHILDREN, childrenQuantityClicks);
 
         SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(searchHotelScreen.filterButtons.getQuantityInput(FilterButtonsScreen.Id.ROOMS), MAX_ROOMS_QUANTITY);
-        softAssert.assertEquals(searchHotelScreen.filterButtons.getQuantityInput(FilterButtonsScreen.Id.ADULTS), MAX_ADULT_PEOPLE_QUANTITY);
-        softAssert.assertEquals(searchHotelScreen.filterButtons.getQuantityInput(FilterButtonsScreen.Id.CHILDREN), MAX_CHILDREN_QUANTITY);
+        softAssert.assertEquals(searchHotelScreen.filterButtons.getQuantityInput(Id.ROOMS), MAX_ROOMS_QUANTITY);
+        softAssert.assertEquals(searchHotelScreen.filterButtons.getQuantityInput(Id.ADULTS), MAX_ADULT_PEOPLE_QUANTITY);
+        softAssert.assertEquals(searchHotelScreen.filterButtons.getQuantityInput(Id.CHILDREN), MAX_CHILDREN_QUANTITY);
     }
 }
