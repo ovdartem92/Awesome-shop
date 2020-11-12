4925985573eb06766a2cff3418323095fced8384
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-11-12 14:14:47 +0300

    refactor: add minor changes

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -8,9 +8,11 @@ import pages.net.skyscanner.profileScreen.ProfileScreen;
 public class AccountService {
     private static final String EMAIL_FIELD_LOCATOR = "//input[@id='email']";
     private static final String PASSWORD_FIELD_LOCATOR = "//input[@id='password']";
+    private LogInScreen logInScreen = new LogInScreen();
+    private ProfileScreen profileScreen = new ProfileScreen();
 
     public void logIn(User user) {
-        LogInScreen logInScreen = new LogInScreen().clickContinueWithEmailButton();
+        logInScreen.clickContinueWithEmailButton();
         AbstractScreen.typeTextToElement(EMAIL_FIELD_LOCATOR, user.getEmail());
         logInScreen.clickNextButton();
         AbstractScreen.typeTextToElement(PASSWORD_FIELD_LOCATOR, user.getPassword());
@@ -20,7 +22,7 @@ public class AccountService {
     }
 
     public void logOut() {
-        new ProfileScreen()
+        profileScreen
                 .clickLogOutButton()
                 .clickConfirmLogOutButton();
     }
