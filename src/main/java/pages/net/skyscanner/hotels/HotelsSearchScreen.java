4420f9890caf5b0645c70f982603762a67d5e0d0
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-13 19:21:18 +0300

    fix: change methods in HotelsSearchScreen

--- src/main/java/pages/net/skyscanner/hotels/HotelsSearchScreen.java
+++ src/main/java/pages/net/skyscanner/hotels/HotelsSearchScreen.java
@@ -5,6 +5,9 @@ import pages.net.skyscanner.hotels.source.FilterButtonsScreen;
 import service.HotelSearchService;
 
 public class HotelsSearchScreen extends AbstractScreen {
+    private static final String DESTINATION_OR_HOSTEL_NAME_INPUT_LOCATOR = "//input[@name='destination-autosuggest']";
+    private static final String FIRST_ELEMENT_OF_DROPDOWN_LOCATOR = "//input[@aria-activedescendant='react-autowhatever-1--item-0']";
+    private static final String GUESTS_AND_ROOMS_INPUT_LOCATOR = "//input[@id='guests-rooms']";
     private static final String HOTELS_SEARCH_BUTTON_LOCATOR = "//button[@data-test-id='search-button']";
     public FilterButtonsScreen filterButtons;
 
@@ -17,12 +20,27 @@ public class HotelsSearchScreen extends AbstractScreen {
     }
 
     public HotelsSearchScreen addDestinations(String destination) {
-        HotelSearchService.addDestination(destination);
+        new HotelSearchService().addDestination(destination);
+        return this;
+    }
+
+    public HotelsSearchScreen typeTextToDestinationInput(String destination) {
+        typeTextToElement(DESTINATION_OR_HOSTEL_NAME_INPUT_LOCATOR, destination);
+        return this;
+    }
+
+    public HotelsSearchScreen clickOnFirstDropdownInput() {
+        clickOnElement(FIRST_ELEMENT_OF_DROPDOWN_LOCATOR);
         return this;
     }
 
     public HotelsResultScreen clickToSearchHotelsButton() {
         clickOnElement(HOTELS_SEARCH_BUTTON_LOCATOR);
         return new HotelsResultScreen();
+    }
+
+    public HotelsSearchScreen clickOnGuestAndRoomsInput() {
+        clickOnElement(GUESTS_AND_ROOMS_INPUT_LOCATOR);
+        return this;
     }
 }
