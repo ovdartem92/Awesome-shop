2f71dc417a06b19277e7bb3afe81bf598a2498b8
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-11-13 18:00:39 +0300

    refactor: change BeforeClass to BeforeMethod

--- src/test/java/LogInTest.java
+++ src/test/java/LogInTest.java
@@ -1,30 +1,30 @@
 import model.User;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.AbstractScreen;
 import pages.net.skyscanner.elements.HeaderScreen;
 import service.AccountService;
 import service.UserBuilder;
 
 public class LogInTest extends BaseTest {
-    private final HeaderScreen HEADER_SCREEN = new HeaderScreen();
     private final AccountService ACCOUNT_SERVICE = new AccountService();
     private User user;
 
-    @BeforeClass(description = "Click on login button")
+    @BeforeMethod(description = "Click on login button")
     public void navigateToLoginScreen() {
-        HEADER_SCREEN.clickLoginButton();
+        AbstractScreen.header.clickLoginButton();
     }
 
     @Test(description = "check login with valid data")
     public void logInWithValidDataTest() {
         ACCOUNT_SERVICE.logIn(user = UserBuilder.getUserWithValidPassword());
-        Assert.assertTrue(HEADER_SCREEN.isAccountButtonDisplayed(), "Oops, something went wrong. You're not logIn");
+        Assert.assertTrue(AbstractScreen.header.isAccountButtonDisplayed(), "Oops, something went wrong. You're not logIn");
     }
 
     @Test(description = "check login with invalid data")
     public void logInWithInValidDataTest() {
         ACCOUNT_SERVICE.logIn(user = UserBuilder.getUserWithInvalidPassword());
-        Assert.assertTrue(HEADER_SCREEN.isLoginButtonDisplayed(), "Oops, something went wrong.");
+        Assert.assertTrue(AbstractScreen.header.isLoginButtonDisplayed(), "Oops, something went wrong.");
     }
 }
