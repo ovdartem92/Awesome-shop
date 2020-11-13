3287e11063740f400558d9e166b8239806c0fdb3
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-13 16:54:40 +0300

    fix: change methods in FilterButtonsScreen

--- src/main/java/pages/net/skyscanner/hotels/source/FilterButtonsScreen.java
+++ src/main/java/pages/net/skyscanner/hotels/source/FilterButtonsScreen.java
@@ -1,29 +1,33 @@
 package pages.net.skyscanner.hotels.source;
 
 import pages.AbstractScreen;
-import org.apache.commons.lang3.StringUtils;
 import service.HotelSearchService;
 
+import static service.HotelSearchService.Title.DECREASE;
+import static service.HotelSearchService.Title.INCREASE;
+
 public class FilterButtonsScreen extends AbstractScreen {
-    public enum Title {DECREASE, INCREASE}
+    public void maxClickButton(HotelSearchService.Id id) {
+        String locator = HotelSearchService.getStringLocatorForFilterButton(INCREASE, id);
+        HotelSearchService.clickButtonUntilThenDisabled(locator);
+    }
 
-    public enum Id {ROOMS, ADULTS, CHILDREN}
+    public void minClickButton(HotelSearchService.Id id) {
+        String locator = HotelSearchService.getStringLocatorForFilterButton(DECREASE, id);
+        HotelSearchService.clickButtonUntilThenDisabled(locator);
+    }
 
-    private static final String GUESTS_AND_ROOM_SECTION = "//div[starts-with(@class, 'BpkSectionListItem')]";
-    private static final String BUTTON_LOCATOR = "//button[@aria-controls='%s'][@title='%s']";
-    private static final String QUANTITY_INPUT_LOCATOR = "//input[@id='%s']";
-
-    public void clickButton(Title title, Id id) {
-        String locator = String.format(BUTTON_LOCATOR, id.toString().toLowerCase(), StringUtils.capitalize(title.toString().toLowerCase()));
+    public void clickButton(HotelSearchService.Title title, HotelSearchService.Id id) {
+        String locator = HotelSearchService.getStringLocatorForFilterButton(title, id);
         HotelSearchService.clickAddItemButton(locator);
     }
 
-    public void clickButton(Title title, Id id, int quantity) {
+    public void clickButton(HotelSearchService.Title title, HotelSearchService.Id id, int quantity) {
         for (int i = 0; i < quantity; i++)
             clickButton(title, id);
     }
 
-    public int getQuantityInput(Id id) {
-        return HotelSearchService.getQuantityItemInput(String.format(QUANTITY_INPUT_LOCATOR, id.toString().toLowerCase()));
+    public int getQuantityInput(HotelSearchService.Id id) {
+        return HotelSearchService.getQuantityItemInput(HotelSearchService.getStringLocatorForFilterInput(id));
     }
 }
