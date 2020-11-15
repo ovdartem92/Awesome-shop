9ad60d0278e22c32a193e781b3892c6a9604cdd5
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-15 14:20:09 +0300

    refactor: change typeTextToElement method

--- src/main/java/pages/AbstractScreen.java
+++ src/main/java/pages/AbstractScreen.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import pages.net.skyscanner.elements.CaptchaScreen;
 import pages.net.skyscanner.elements.HeaderScreen;
@@ -40,7 +41,7 @@ public abstract class AbstractScreen {
 
     public static void typeTextToElement(String locatorPath, String text) {
         WebElement element = getElement(locatorPath);
-        element.clear();
+        element.sendKeys(Keys.DELETE);
         element.sendKeys(text);
         logger.info("Type text {} to element with next xpath: {}", text, locatorPath);
     }
