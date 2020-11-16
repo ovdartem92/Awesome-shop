765b567630ba3aabb49bc0b6afc29b15eea8d04a
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-16 16:57:53 +0300

    ref: solved micro-conflicts

--- src/main/java/pages/net/skyscanner/cars/CarsSearchScreen.java
+++ src/main/java/pages/net/skyscanner/cars/CarsSearchScreen.java
@@ -6,11 +6,10 @@ import org.openqa.selenium.support.ui.Select;
 import pages.AbstractScreen;
 
 public class CarsSearchScreen extends AbstractScreen {
-    private static final String CAR_HEADLINE_LOCATOR = "//button[contains(@class,'Search')]";
+    private static final String CAR_HEADLINE_LOCATOR = "//div[contains(@class,'controls-title')]";
     private static final String PICKUP_LOCATION_LOCATOR = "//input[@id='carhire-search-controls-location-pick-up']";
     private static final String DROP_OFF_LOCATION_LOCATOR = "//input[@id='carhire-search-controls-location-drop-off']";
     private static final String RETURN_CAR_TO_A_DIFFERENT_LOCATION_CHECKBOX_LOCATOR = "//input[@id='carhire-search-controls-different-drop-off-docked']";
-    private static final String FIRST_ELEMENT_OF_DROPDOWN_LOCATOR = "//input[@aria-activedescendant='react-autowhatever-1--item-0']";
     private static final String SEARCH_BUTTON_LOCATOR = "//button[@id='carhire-search-controls-search-button']";
     private static final String DRIVER_AGE_CHECKBOX_LOCATOR = "//input[@id='carhire-search-controls-age-between']";
     private static final String DRIVER_AGE_SELECT_LOCATOR = "//select[@id='carhire-search-controls-age-selector']";
