8afe974be26f9d8aeae9327156b39c4efea7327a
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-17 16:35:40 +0300

    fix: change methods in HotelsSearchScreen class

--- src/main/java/pages/net/skyscanner/hotels/HotelsSearchScreen.java
+++ src/main/java/pages/net/skyscanner/hotels/HotelsSearchScreen.java
@@ -2,11 +2,11 @@ package pages.net.skyscanner.hotels;
 
 import org.apache.commons.lang3.StringUtils;
 import pages.AbstractScreen;
-import pages.net.skyscanner.hotels.hotelsService.HotelsSearchService;
+import service.HotelsSearchService;
 
 public class HotelsSearchScreen extends AbstractScreen {
-    public enum Title {DECREASE, INCREASE}
-    public enum Id {ROOMS, ADULTS, CHILDREN}
+    public enum FilterButtonsTitle {DECREASE, INCREASE}
+    public enum FilterButtonsId {ROOMS, ADULTS, CHILDREN}
     private static final String DESTINATION_OR_HOSTEL_NAME_INPUT_LOCATOR = "//input[@name='destination-autosuggest']";
     private static final String FIRST_ELEMENT_OF_DROPDOWN_LOCATOR = "//input[@aria-activedescendant='react-autowhatever-1--item-0']";
     private static final String HOTELS_SEARCH_BUTTON_LOCATOR = "//button[@data-test-id='search-button']";
@@ -20,58 +20,53 @@ public class HotelsSearchScreen extends AbstractScreen {
         return getTextOnElement(HOTELS_SEARCH_BUTTON_LOCATOR);
     }
 
-    public HotelsSearchScreen addDestinations(String destination) {
-        typeTextToDestinationInput(destination);
-        clickOnFirstDropdownInput();
-        return this;
-    }
-
     public HotelsSearchScreen typeTextToDestinationInput(String destination) {
         typeTextToElement(DESTINATION_OR_HOSTEL_NAME_INPUT_LOCATOR, destination);
+        clickFirstDropdownInput();
         return this;
     }
 
-    public HotelsSearchScreen clickOnFirstDropdownInput() {
+    public HotelsSearchScreen clickFirstDropdownInput() {
         clickOnElement(FIRST_ELEMENT_OF_DROPDOWN_LOCATOR);
         return this;
     }
 
-    public HotelsResultScreen clickToSearchHotelsButton() {
+    public HotelsResultScreen clickSearchHotelsButton() {
         clickOnElement(HOTELS_SEARCH_BUTTON_LOCATOR);
         return new HotelsResultScreen();
     }
 
-    public HotelsSearchScreen clickOnGuestAndRoomsInput() {
+    public HotelsSearchScreen clickGuestAndRoomsInput() {
         clickOnElement(GUESTS_AND_ROOMS_INPUT_LOCATOR);
         return this;
     }
 
-    public HotelsSearchScreen clickOnDoneButton() {
+    public HotelsSearchScreen clickDoneFilterButton() {
         clickOnElement(DONE_BUTTON_LOCATOR);
         return this;
     }
 
-    public HotelsSearchScreen maxClickButton(Id id) {
-        String locator = getStringLocatorForFilterButton(Title.INCREASE, id);
-        hotelsSearchService.clickButtonUntilThenDisabled(locator);
+    public HotelsSearchScreen clickMaxIncreaseFilterButton(FilterButtonsId id) {
+        String locator = getLocatorForFilterButton(FilterButtonsTitle.INCREASE, id);
+        hotelsSearchService.clickFilterButtonUntilDisabled(locator);
         return this;
     }
 
-    public HotelsSearchScreen minClickButton(Id id) {
-        String locator = getStringLocatorForFilterButton(Title.DECREASE, id);
-        hotelsSearchService.clickButtonUntilThenDisabled(locator);
+    public HotelsSearchScreen clickMaxDecreaseFilterButton(FilterButtonsId id) {
+        String locator = getLocatorForFilterButton(FilterButtonsTitle.DECREASE, id);
+        hotelsSearchService.clickFilterButtonUntilDisabled(locator);
         return this;
     }
 
-    public int getQuantityInput(Id id) {
-        return hotelsSearchService.getQuantityItemInput(getStringLocatorForFilterInput(id));
+    public int getQuantityOfFilterInput(FilterButtonsId id) {
+        return hotelsSearchService.getQuantityOfFilterInput(getLocatorForFilterInput(id));
     }
 
-    private String getStringLocatorForFilterButton(Title title, Id id) {
+    private String getLocatorForFilterButton(FilterButtonsTitle title, FilterButtonsId id) {
         return String.format(BUTTON_LOCATOR, id.toString().toLowerCase(), StringUtils.capitalize(title.toString().toLowerCase()));
     }
 
-    private String getStringLocatorForFilterInput(Id id) {
+    private String getLocatorForFilterInput(FilterButtonsId id) {
         return String.format(QUANTITY_INPUT_LOCATOR, id.toString().toLowerCase());
     }
 }
