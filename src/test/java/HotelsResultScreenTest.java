360af4c5efaf64bf79b456f9d03d7ba5cab2f251
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-18 17:45:15 +0300

    feat: add test for sorting hotels by rating

---
+++ src/test/java/HotelsResultScreenTest.java
@@ -0,0 +1,28 @@
+import model.Hotel;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.AbstractScreen;
+import pages.net.skyscanner.hotels.HotelsResultScreen;
+import utils.Constants;
+
+import java.util.List;
+
+public class HotelsResultScreenTest extends BaseTest {
+    HotelsResultScreen hotelsResultScreen;
+
+    @BeforeClass(description = "click on hotels button")
+    public void navigateToHotelsSearchPage() {
+        HotelsResultScreen hotelsResultScreen = AbstractScreen.header.clickHotelButton()
+                .typeTextToDestinationInput(Constants.TURIN)
+                .clickSearchHotelsButton();
+    }
+
+    @Test(description = "checking hotel sorting by rating")
+    public void checkRatingSort() {
+        hotelsResultScreen.clickToGuestRattingSortButton();
+        List<Hotel> hotels = hotelsResultScreen.getHotels();
+        Assert.assertTrue(hotelsResultScreen.isHotelSortedByRating(hotels),
+                "the sorting of collection values by rating does not match");
+    }
+}
