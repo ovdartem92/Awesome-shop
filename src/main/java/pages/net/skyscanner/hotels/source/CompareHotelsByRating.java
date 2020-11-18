6ee324f6bcf4cbab548ac947dd6990c8611a394d
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-18 17:22:10 +0300

    feat: add price and rating comparators

---
+++ src/main/java/pages/net/skyscanner/hotels/source/CompareHotelsByRating.java
@@ -0,0 +1,15 @@
+package pages.net.skyscanner.hotels.source;
+
+import model.Hotel;
+
+import java.util.Comparator;
+
+public class CompareHotelsByRating implements Comparator<Hotel> {
+
+    @Override
+    public int compare(Hotel h1, Hotel h2) {
+        if (h1.getRating() > h2.getRating()) return -1;
+        else if (h1.getRating() < h2.getRating()) return 1;
+        else return 0;
+    }
+}
