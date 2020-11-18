f46e6216be599a96f08543537115333ee7467159
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-18 17:23:14 +0300

    feat: add methods and fields in HotelsResultScreen

--- src/main/java/pages/net/skyscanner/hotels/HotelsResultScreen.java
+++ src/main/java/pages/net/skyscanner/hotels/HotelsResultScreen.java
@@ -1,6 +1,39 @@
 package pages.net.skyscanner.hotels;
 
+import model.Hotel;
 import pages.AbstractScreen;
+import service.HotelsResultService;
+
+import java.util.List;
 
 public class HotelsResultScreen extends AbstractScreen {
+    private final static String GUEST_RATING_SORT_BUTTON_LOCATOR = "//button[@data-test-id='search-sort-rating']";
+    private final static String PRICE_SORT_BUTTON_LOCATOR = "//button[@data-test-id='search-sort-price']";
+
+    public List<Hotel> getHotels() {
+        HotelsResultService.isWaitingModalInvisible();
+        return new HotelsResultService().getHotels();
+    }
+
+    public boolean isHotelSortedByRating(List<Hotel> hotels) {
+        HotelsResultService.isWaitingModalInvisible();
+        return new HotelsResultService().isHotelSortedByRating(hotels);
+    }
+
+    public int getFoundHotelsQuantity() {
+        HotelsResultService.isWaitingModalInvisible();
+        return HotelsResultService.getFoundHotelsQuantity();
+    }
+
+    public HotelsResultScreen clickToGuestRattingSortButton() {
+        HotelsResultService.isWaitingModalInvisible();
+        clickOnElement(GUEST_RATING_SORT_BUTTON_LOCATOR);
+        return this;
+    }
+
+    public HotelsResultScreen clickToPriceSortButton() {
+        HotelsResultService.isWaitingModalInvisible();
+        clickOnElement(PRICE_SORT_BUTTON_LOCATOR);
+        return this;
+    }
 }
