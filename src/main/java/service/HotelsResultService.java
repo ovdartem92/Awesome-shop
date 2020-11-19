9275f91224134496f8ba9af89df139a47b3c835b
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-19 11:41:29 +0300

    refactor: delete fields and method in HotelsResultService

--- src/main/java/service/HotelsResultService.java
+++ src/main/java/service/HotelsResultService.java
@@ -16,59 +16,38 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import static pages.AbstractScreen.getElements;
-import static pages.AbstractScreen.getTextOnElement;
-import static service.WaitManager.waitForInvisibilityOfElementLocated;
-
 public class HotelsResultService {
     public static Logger logger = LogManager.getRootLogger();
-    private final String HOTEL_STRING = "HOTEL [name: '%s', rating: '%s', price: '%s']";
-    protected static final String WAITING_MODAL_VIEW = "//div[starts-with(@class, 'WaitingModal_WaitingModal')]";
-    private final static String SEARCH_SUMMARY_LOCATOR = "//p[@data-test-id='search-summary']";
-    private final static String HOTEL_BLOCK_LOCATOR = "//section//div[@role='button']";
-    private final static String HOTEL_NAME_LOCATOR = ".//span[@data-test-id='hotel-name']";
-    private final static String HOTEL_RATING_LOCATOR = ".//div[1]//div[3]/span";
-    private final static String HOTEL_PRICE_LOCATOR = ".//div[3]//div[contains(@class, 'HotelCard_HotelCard__priceVariant')]";
     private HotelsResultScreen hotelsResultScreen;
 
     public HotelsResultService(HotelsResultScreen hotelsResultScreen) {
         this.hotelsResultScreen = hotelsResultScreen;
     }
 
-    public static boolean isWaitingModalInvisible() {
-        return waitForInvisibilityOfElementLocated(WAITING_MODAL_VIEW);
-    }
-
     public List<Hotel> getHotels() {
         List<Hotel> hotels = new ArrayList<>();
-        List<WebElement> hotelElements = getElements(HOTEL_BLOCK_LOCATOR);
+        List<WebElement> hotelElements = hotelsResultScreen.getElements();
 
         for (WebElement hotel : hotelElements) {
             String name = getHotelName(hotel);
             double rating = getHotelRating(hotel);
             int price = getHotelPrice(hotel);
-            logger.info(String.format(HOTEL_STRING, name, rating, price));
+            logger.info("HOTEL [name: {}, rating: {}, price: {}]", name, rating, price);
             hotels.add(new Hotel(name, rating, price));
         }
         return hotels;
     }
 
-    public static int getFoundHotelsQuantity() {
-        HotelsResultService.isWaitingModalInvisible();
-        String searchSummaryLine = getTextOnElement(SEARCH_SUMMARY_LOCATOR);
-        return Integer.parseInt(StringUtils.getMatcherByIndex(searchSummaryLine, "\\w+", 0));
-    }
-
     private String getHotelName(WebElement hotel) {
-        return getText(hotel, HOTEL_NAME_LOCATOR);
+        return getText(hotel, hotelsResultScreen.HOTEL_NAME_LOCATOR);
     }
 
     private double getHotelRating(WebElement hotel) {
-        return Double.parseDouble(getText(hotel, HOTEL_RATING_LOCATOR));
+        return Double.parseDouble(getText(hotel, hotelsResultScreen.HOTEL_RATING_LOCATOR));
     }
 
     private int getHotelPrice(WebElement hotel) {
-        return Integer.parseInt(StringUtils.getMatcherByIndex(getText(hotel, HOTEL_PRICE_LOCATOR), "\\w+", 0));
+        return Integer.parseInt(StringUtils.getMatcherByIndex(getText(hotel, hotelsResultScreen.HOTEL_PRICE_LOCATOR), "\\w+", 0));
     }
 
     private String getText(WebElement element, String locator) {
