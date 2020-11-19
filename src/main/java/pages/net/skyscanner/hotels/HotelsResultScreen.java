74ec32638453b87a39f4a16db1c5d153ef89c2fd
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-19 11:41:46 +0300

    refactor: add fields and method in HotelsResultScreen

--- src/main/java/pages/net/skyscanner/hotels/HotelsResultScreen.java
+++ src/main/java/pages/net/skyscanner/hotels/HotelsResultScreen.java
@@ -1,39 +1,51 @@
 package pages.net.skyscanner.hotels;
 
 import model.Hotel;
+import org.openqa.selenium.WebElement;
 import pages.AbstractScreen;
 import service.HotelsResultService;
 
 import java.util.List;
 
+import static service.WaitManager.waitForInvisibilityOfElementLocated;
+
 public class HotelsResultScreen extends AbstractScreen {
+    public final static String HOTEL_NAME_LOCATOR = ".//span[@data-test-id='hotel-name']";
+    public final static String HOTEL_RATING_LOCATOR = ".//div[1]//div[3]/span";
+    public final static String HOTEL_PRICE_LOCATOR = ".//div[3]//div[contains(@class, 'HotelCard_HotelCard__priceVariant')]";
+    private static final String WAITING_MODAL_VIEW = "//div[starts-with(@class, 'WaitingModal_WaitingModal')]";
+    private final static String HOTEL_BLOCK_LOCATOR = "//section//div[@role='button']";
     private final static String GUEST_RATING_SORT_BUTTON_LOCATOR = "//button[@data-test-id='search-sort-rating']";
     private final static String PRICE_SORT_BUTTON_LOCATOR = "//button[@data-test-id='search-sort-price']";
     private HotelsResultService hotelsResultService = new HotelsResultService(this);
 
+    public static boolean isWaitingModalInvisible() {
+        return waitForInvisibilityOfElementLocated(WAITING_MODAL_VIEW);
+    }
+
+    public List<WebElement> getElements() {
+        isWaitingModalInvisible();
+        return getElements(HOTEL_BLOCK_LOCATOR);
+    }
+
     public List<Hotel> getHotels() {
-        hotelsResultService.isWaitingModalInvisible();
+        isWaitingModalInvisible();
         return hotelsResultService.getHotels();
     }
 
     public boolean isHotelSortedByRating(List<Hotel> hotels) {
-        hotelsResultService.isWaitingModalInvisible();
+        isWaitingModalInvisible();
         return hotelsResultService.isHotelSortedByRating(hotels);
     }
 
-    public int getFoundHotelsQuantity() {
-        hotelsResultService.isWaitingModalInvisible();
-        return hotelsResultService.getFoundHotelsQuantity();
-    }
-
     public HotelsResultScreen clickToGuestRattingSortButton() {
-        hotelsResultService.isWaitingModalInvisible();
+        isWaitingModalInvisible();
         clickOnElement(GUEST_RATING_SORT_BUTTON_LOCATOR);
         return this;
     }
 
     public HotelsResultScreen clickToPriceSortButton() {
-        hotelsResultService.isWaitingModalInvisible();
+        isWaitingModalInvisible();
         clickOnElement(PRICE_SORT_BUTTON_LOCATOR);
         return this;
     }
