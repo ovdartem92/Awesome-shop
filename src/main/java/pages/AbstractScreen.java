ecd304f580e42483a63c418a672c2aace8cd51cc
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-19 15:23:24 +0300

    add: create javadoc for AbstractScreen class

--- src/main/java/pages/AbstractScreen.java
+++ src/main/java/pages/AbstractScreen.java
@@ -11,33 +11,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractScreen {
+
+    /**
+     * This variable is xpath locator of web element, which appears when we fill in the search field.
+     * It's first element of drop-down list, and this element contains needed value for input field.
+     * It's common locator for all search pages.
+     */
     protected static final String FIRST_ELEMENT_OF_DROPDOWN_LOCATOR = "//input[@aria-activedescendant='react-autowhatever-1--item-0']";
     protected static final Logger logger = LogManager.getRootLogger();
 
+    /**
+     * This method performs getting of needed web element.
+     * @param locatorPath the param according to witch we get web element.
+     */
     public static WebElement getElement(String locatorPath) {
         CaptchaScreen.checkCaptchaOnPage();
         return WaitManager.waitForElementLocated(locatorPath);
     }
 
+    /**
+     * This method performs getting attribute value of needed web element.
+     * @param locatorPath the param according to witch we get web element.
+     * @param attribute the value of needed attribute.
+     * @return attribute's text.
+     */
     public static String getAttributeValueOnElement(String locatorPath, String attribute) {
         return WaitManager.waitForElementLocated(locatorPath).getAttribute(attribute);
     }
 
+    /**
+     * This method performs getting of needed web elements list.
+     * @param locatorPath the param according to witch we get web elements.
+     */
     public static List<WebElement> getElements(String locatorPath) {
         CaptchaScreen.checkCaptchaOnPage();
         return WaitManager.waitForAllElementsLocated(locatorPath);
     }
 
+
+    /**
+     * This method performs getting of needed web element, checks that element is clickable and click on it.
+     * @param locatorPath the param according to witch we get web element.
+     */
     public static void clickOnElement(String locatorPath) {
         CaptchaScreen.checkCaptchaOnPage();
         WaitManager.waitForElementToBeClickable(locatorPath).click();
         logger.info("Click on element with next xpath: {}", locatorPath);
     }
 
+    /**
+     * This method performs getting text value of needed web element.
+     * @param locatorPath the param according to witch we get web element.
+     */
     public static String getTextOnElement(String locatorPath) {
         return WaitManager.waitForElementLocated(locatorPath).getText();
     }
 
+    /**
+     * This method performs typing text to needed web element, previously clears input field.
+     * @param locatorPath the param according to witch we get web element.
+     */
     public static void typeTextToElement(String locatorPath, String text) {
         WebElement element = getElement(locatorPath);
         element.sendKeys(Keys.DELETE);
@@ -45,6 +78,11 @@ public abstract class AbstractScreen {
         logger.info("Type text {} to element with next xpath: {}", text, locatorPath);
     }
 
+    /**
+     * This method performs getting text value of needed web elements list.
+     * @param locatorPath the param according to witch we get web elements.
+     * @return the list of strings, which contains text from web elements.
+     */
     public static List<String> getTextFromElements(String locatorPath) {
         List<String> textFromElements = new ArrayList<>();
         for (WebElement element : getElements(locatorPath)) {
