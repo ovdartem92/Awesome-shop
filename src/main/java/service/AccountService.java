89ac7eb1b485a44388b36b7766eb81b4cc3f8992
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-11-19 12:04:32 +0300

    refactor: remove ProfileScreen

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -2,14 +2,12 @@ package service;
 
 import model.User;
 import pages.net.skyscanner.elements.LogInScreen;
-import pages.net.skyscanner.profile_screen.ProfileScreen;
 
 /**
  * This is a service class that performs logic during login and logout.
  */
 public class AccountService {
     private final LogInScreen logInScreen = new LogInScreen();
-    private final ProfileScreen profileScreen = new ProfileScreen();
 
     /**
      * This method performs actions for the login to the system.
@@ -25,14 +23,5 @@ public class AccountService {
                 .clickLogInButton()
                 .waitMarketingConsentButtonAvailable()
                 .clickCloseModalButton();
-    }
-
-    /**
-     * This method performs actions for logging out of the system.
-     */
-    public void logOut() {
-        profileScreen
-                .clickLogOutButton()
-                .clickConfirmLogOutButton();
     }
 }
