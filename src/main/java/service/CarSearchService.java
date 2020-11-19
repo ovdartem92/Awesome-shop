54b10bc1f9a85d248b2f1479f3dd9eba598203aa
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-19 11:26:25 +0300

    add: create javadoc for CarSearchService, searchCarUsingLocationsData()

--- src/main/java/service/CarSearchService.java
+++ src/main/java/service/CarSearchService.java
@@ -3,14 +3,26 @@ package service;
 import pages.net.skyscanner.cars.CarsSearchResultsScreen;
 import pages.net.skyscanner.elements.HeaderScreen;
 
+/**
+ * This is a service class that performs actions to fill in the data for the cars search and performs the search itself.
+ */
+
 public class CarSearchService {
     private final HeaderScreen headerScreen = new HeaderScreen();
 
-    public CarsSearchResultsScreen searchCarUsingLocationsData(String pickUpLock, String dropOffLock) {
+    /**
+     * This is a method that performs search cars using locations data.
+     *
+     * @param pickUpLoc  is represents necessary location for pick up car.
+     * @param dropOffLoc is represents necessary location for leave car.
+     * @return the screen with search results.
+     */
+
+    public CarsSearchResultsScreen searchCarUsingLocationsData(String pickUpLoc, String dropOffLoc) {
         headerScreen.clickCarButton()
                 .choiceReturnCarToADifferentLocation()
-                .setUpPickUpLocation(pickUpLock)
-                .setUpDropOffLocation(dropOffLock)
+                .setUpPickUpLocation(pickUpLoc)
+                .setUpDropOffLocation(dropOffLoc)
                 .clickSearchButton();
         return new CarsSearchResultsScreen();
     }
