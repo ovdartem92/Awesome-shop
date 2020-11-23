623bad3a6861de16bec9fe114e3883b70bddc334
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-23 22:43:13 +0300

    fix: make method in CarSearchService void and edit test

--- src/main/java/service/CarSearchService.java
+++ src/main/java/service/CarSearchService.java
@@ -1,6 +1,5 @@
 package service;
 
-import pages.net.skyscanner.cars.CarsSearchResultsScreen;
 import pages.net.skyscanner.elements.HeaderScreen;
 
 import java.util.List;
@@ -8,13 +7,12 @@ import java.util.List;
 public class CarSearchService {
     private HeaderScreen headerScreen = new HeaderScreen();
 
-    public CarsSearchResultsScreen searchCarUsingLocationsData(String pickUpLock, String dropOffLock) {
+    public void searchCarUsingLocationsData(String pickUpLock, String dropOffLock) {
         headerScreen.clickCarButton()
                 .choiceReturnCarToADifferentLocation()
                 .setUpPickUpLocation(pickUpLock)
                 .setUpDropOffLocation(dropOffLock)
                 .clickSearchButton();
-        return new CarsSearchResultsScreen();
     }
 
     public List<String> getCarClassTypeInfoFromSearchResults(String pickUpLock, String carClass, String carType) {
