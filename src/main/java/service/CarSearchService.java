a551993838b1b5e776d3f9116a1af47775131b83
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-26 18:37:53 +0300

    fix: make car service method searchCarUsingLocationsData() void and edit test

--- src/main/java/service/CarSearchService.java
+++ src/main/java/service/CarSearchService.java
@@ -1,18 +1,16 @@
 package service;
 
-import pages.net.skyscanner.cars.CarsSearchResultsScreen;
 import pages.net.skyscanner.elements.HeaderScreen;
 
 public class CarSearchService {
     HeaderScreen headerScreen = new HeaderScreen();
 
-    public CarsSearchResultsScreen searchCarUsingLocationsData(String pickUpLock, String dropOffLock) {
+    public void searchCarUsingLocationsData(String pickUpLock, String dropOffLock) {
         headerScreen.clickCarButton()
                 .choiceReturnCarToADifferentLocation()
                 .setUpPickUpLocation(pickUpLock)
                 .setUpDropOffLocation(dropOffLock)
                 .clickSearchButton();
-        return new CarsSearchResultsScreen();
     }
 
     public void searchCarUsingAgeAndPickUpLocation(String pickUpLock, String age) {
