dfad8ed7ceb30fe428de015044232977f6f7cab6
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-26 18:54:21 +0300

    ref: get changes from master

--- src/main/java/pages/net/skyscanner/cars/CarsSearchResultsScreen.java
+++ src/main/java/pages/net/skyscanner/cars/CarsSearchResultsScreen.java
@@ -4,9 +4,12 @@ import pages.AbstractScreen;
 import service.WaitManager;
 import utils.StringUtils;
 
+import java.util.List;
+
 public class CarsSearchResultsScreen extends AbstractScreen {
     private static final String CLASS_TEXT = "class";
     private static final String TYPE_TEXT = "type";
+    private static final String ALERT_AGE_MESSAGE = "//span[contains(text(), 'Driver aged under 25')]";
     private static final String CAR_SEARCH_SUMMARY_ROUTE_LOCATOR = "//div[@id='carhire-search-summary-route']";
     private static final String CAR_GROUP_PANEL_INFO_LOCATOR_SUMMARY = "//div[contains(@class, 'CarGroupPanel_car-info')]";
     private static final String CAR_CHECKBOX_LOCATOR = "//div[@id='car-%s']//input[@type='checkbox' and @aria-label='%s']";
