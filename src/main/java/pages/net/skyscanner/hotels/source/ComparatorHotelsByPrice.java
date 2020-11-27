c260b5b06a1c868ae5beab6265958bd8f7cc4f9b
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-11-27 16:02:09 +0300

    feat: add ComparatorHotelsByPrice

---
+++ src/main/java/pages/net/skyscanner/hotels/source/ComparatorHotelsByPrice.java
@@ -0,0 +1,15 @@
+package pages.net.skyscanner.hotels.source;
+
+import model.Hotel;
+
+import java.util.Comparator;
+
+public class ComparatorHotelsByPrice implements Comparator<Hotel> {
+
+    @Override
+    public int compare(Hotel h1, Hotel h2) {
+        if (h1.getPrice() > h2.getPrice()) return 1;
+        else if (h1.getPrice() < h2.getPrice()) return -1;
+        else return 0;
+    }
+}
