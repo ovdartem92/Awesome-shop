50d4fc923ce57318e4746a994993f9d8962f8971
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-11-27 13:00:26 +0300

    refactor: added changes according to comments

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -11,4 +11,6 @@ public final class Constants {
     public static final String TEXT_ON_CAR_SEARCH_PAGE = "Find your ride";
     public static final String TEXT_ON_FLIGHTS_SEARCH_PAGE = "Search flights";
     public static final String TEXT_ON_HOTEL_SEARCH_PAGE = "Search hotels";
+    public static final String FIRST_NAME = "John";
+    public static final String LAST_NAME = "Smith";
 }
