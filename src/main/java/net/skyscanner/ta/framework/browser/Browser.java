3b1169d4d950ac16c0ccced82e9703cb1a3ddfd8
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-11-30 22:59:18 +0300

    add: add implementation Browser/Driver classes

--- src/main/java/net/skyscanner/ta/framework/browser/Browser.java
+++ src/main/java/net/skyscanner/ta/framework/browser/Browser.java
@@ -1,4 +1,85 @@
 package net.skyscanner.ta.framework.browser;
 
+import org.openqa.selenium.*;
+
+import java.io.File;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 public enum Browser {
+    INSTANCE;
+
+    public static void stop() {
+        WebDriverFactory.closeWebDriver();
+    }
+
+    public static WebDriver getWrappedDriver() {
+        return WebDriverFactory.getWebDriver(BrowserType.valueOf(System.getProperty("browser")));
+    }
+
+    public static void click(By locator) {
+        getWrappedDriver().findElement(locator).click();
+    }
+
+    public static boolean isSelected(By locator) {
+        //implement later
+        return true;
+    }
+
+    public static void select(By locator, String option) {
+        //implement later
+    }
+
+    public static void sendKeys(By locator, CharSequence... keysToSend) {
+        getWrappedDriver().findElement(locator).sendKeys(keysToSend);
+    }
+
+    public void clear(By locator) {
+        getWrappedDriver().findElement(locator).clear();
+    }
+
+    public void reloadPage() {
+        getWrappedDriver().get(getWrappedDriver().getCurrentUrl());
+    }
+
+    public String getText(By locator) {
+        //implement later
+        return "";
+    }
+
+    public File takeScreenshot() {
+        File scrShot = ((TakesScreenshot) getWrappedDriver()).getScreenshotAs(OutputType.FILE);
+//        try {
+//            Path path = Paths.get(".", "target", "screenshots", String.format("%s %s.png", getWrappedDriver().getTitle(), getCurrentTimeAsString()));
+//            FileUtils.copyFile(scrShot, new File(path.toString()));
+//        } catch (IOException e) {
+//            System.out.println(String.format("Failed to save screenshots %s", e.getLocalizedMessage()));
+//        }
+        return scrShot;
+    }
+
+    //temp solution while class StringUtils isn't created
+
+    public static String getCurrentTimeAsString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd_HH-mm-ss");
+        return ZonedDateTime.now().format(formatter);
+    }
+
+    public static WebElement getElement(By locator) {
+        return getWrappedDriver().findElement(locator);
+    }
+
+    public static void openNewTab() {
+        sendKeys(By.tagName("Body"), Keys.CONTROL + "t");
+//        or
+//        ((JavascriptExecutor) getWrappedDriver()).executeScript("window.open();");
+    }
+
+    public static void switchTab(String windowHandle) {
+        //implement later
+    }
+
+    public static void closeTab(String windowHandle) {
+        //implement later
+    }
 }
--- src/main/java/net/skyscanner/ta/framework/browser/WebDriverFactory.java
+++ src/main/java/net/skyscanner/ta/framework/browser/WebDriverFactory.java
@@ -1,4 +1,53 @@
 package net.skyscanner.ta.framework.browser;
 
-public class WebDriverFactory {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.String.format;
+
+public final class WebDriverFactory {
+
+    private static final ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
+
+    private WebDriverFactory() {
+        throw new AssertionError(format("Creation of instance of %s is prohibited.", WebDriverFactory.class));
+    }
+
+    public static WebDriver getWebDriver(BrowserType type) {
+        if (webDriverThreadLocal.get() == null) {
+            switch (type) {
+                case FIREFOX: {
+                    WebDriverManager.firefoxdriver().setup();
+                    webDriverThreadLocal.set(new FirefoxDriver());
+                    break;
+                }
+                case CHROME: {
+                    WebDriverManager.chromedriver().setup();
+                    webDriverThreadLocal.set(new ChromeDriver());
+                    break;
+                }
+                default:
+                    throw new IllegalArgumentException(format("Unexpected browser type: %s", type));
+            }
+        }
+        configureWebDriver(webDriverThreadLocal.get());
+        return webDriverThreadLocal.get();
+    }
+
+    private static void configureWebDriver(WebDriver driver) {
+        driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    public static void closeWebDriver() {
+        for (String handle : webDriverThreadLocal.get().getWindowHandles()) {
+            webDriverThreadLocal.get().switchTo().window(handle);
+            webDriverThreadLocal.get().close();
+        }
+        webDriverThreadLocal.remove();
+    }
 }
