5186a31776f1c9df8e6db706a780c54d4f0da381
Anastasiya Murashka <anastasiya_murashka@epam.com>
2020-11-30 21:58:05 +0300

    add suite and test listeners

--- src/main/java/net/skyscanner/ta/framework/listeners/SuiteListener.java
+++ src/main/java/net/skyscanner/ta/framework/listeners/SuiteListener.java
@@ -1,4 +1,34 @@
 package net.skyscanner.ta.framework.listeners;
 
-public class SuiteListener {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ISuiteResult;
+import org.testng.ITestNGMethod;
+
+public class SuiteListener implements ISuiteListener {
+    private final Logger logger = LogManager.getRootLogger();
+
+    @Override
+    public void onStart(ISuite suite) {
+        logger.info("Test suite - \"{}\" started", suite.getName());
+        logger.info("Suite \"{}\" contains: ", suite.getName());
+        for (ITestNGMethod method : suite.getAllMethods()) {
+            logger.info(method.getMethodName());
+        }
+    }
+
+    @Override
+    public void onFinish(ISuite suite) {
+        logger.info("Test suite - {} finished", suite.getName());
+        for (ISuiteResult result : suite.getResults().values()) {
+            logger.info("This tests was successfully passed: ");
+            logger.info(result.getTestContext().getPassedTests().getAllMethods());
+            logger.info("This tests was failed: ");
+            logger.info(result.getTestContext().getFailedTests().getAllMethods());
+            logger.info("This tests was skipped: ");
+            logger.info(result.getTestContext().getSkippedTests().getAllMethods());
+        }
+    }
 }
--- src/main/java/net/skyscanner/ta/framework/listeners/TestListener.java
+++ src/main/java/net/skyscanner/ta/framework/listeners/TestListener.java
@@ -1,4 +1,72 @@
 package net.skyscanner.ta.framework.listeners;
 
-public class TestListener {
+import driver.Browser;
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import utils.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+public class TestListener implements ITestListener {
+    private final Logger logger = LogManager.getRootLogger();
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        logger.info("Test {} STARTED.\n", iTestResult.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        logger.info("Test {} PASSED.\n", iTestResult.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        logger.info("Test {} FAILED.\n", iTestResult.getName());
+        saveScreenshot(iTestResult);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+        logger.info("Test {} SKIPPED.\n", iTestResult.getName());
+        saveScreenshot(iTestResult);
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+        Objects.requireNonNull(iTestResult, "ITestResult cannot be null");
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+        // unused at the moment
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+        // unused at the moment
+    }
+
+    public void saveScreenshot(ITestResult iTestResult) {
+        File screenCapture = ((TakesScreenshot) Browser
+                .initDriver())
+                .getScreenshotAs(OutputType.FILE);
+        try {
+            Path path = Paths.get(".", "target", "screenshots", String.format("%s %s.png", iTestResult.getName(),
+                    StringUtils.getCurrentTimeAsString()));
+            FileUtils.copyFile(screenCapture, new File(path.toString()));
+        } catch (IOException e) {
+            logger.error("Failed to save screenshots {}", e.getLocalizedMessage());
+        }
+    }
 }
