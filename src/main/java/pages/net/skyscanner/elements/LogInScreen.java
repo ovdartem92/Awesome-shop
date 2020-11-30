0b6a85f9681381eb3e1b360cacf5238cbc2f4b33
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-11-30 13:22:17 +0300

    refactor: added changes

--- src/main/java/pages/net/skyscanner/elements/LogInScreen.java
+++ src/main/java/pages/net/skyscanner/elements/LogInScreen.java
@@ -8,10 +8,10 @@ public class LogInScreen extends AbstractScreen {
     private static final String CONTINUE_WITH_EMAIL_BUTTON_LOCATOR = "//button[@data-testid='login-email-button']";
     private static final String ACCOUNT_DETECTED_BUTTON_LOCATOR = "//button[@data-testid='account-detection-button']";
     private static final String LOG_IN_BUTTON_LOCATOR = "//button[@data-testid='login-button']";
-    private static final String CLOSE_MODAL_LOGIN_WINDOW_BUTTON_LOCATOR = "//button[@title='Close modal']";
-    private static final String MARKETING_CONSENT_BUTTON_LOCATOR = "//button[@data-testid='btn-marketing-consent-cta']";
+    private static final String WRONG_EMAIL_MESSAGE_LOCATOR = "//span[contains(@class,'banner-alert__icon')]";
     private static final String EMAIL_FIELD_LOCATOR = "//input[@id='email']";
     private static final String PASSWORD_FIELD_LOCATOR = "//input[@id='password']";
+    private static final String CLOSE_MODAL_LOCATOR = "//button[@data-testid='modal-close-button']";
 
     public LogInScreen typeTextToEmailField(String txt) {
         typeTextToElement(EMAIL_FIELD_LOCATOR, txt);
@@ -38,13 +38,12 @@ public class LogInScreen extends AbstractScreen {
         return this;
     }
 
-    public LogInScreen waitMarketingConsentButtonAvailable() {
-        WaitManager.isElementVisible(MARKETING_CONSENT_BUTTON_LOCATOR, Browser.SHORT_TIMEOUT_SECONDS);
+    public LogInScreen clickCloseModalButton() {
+        clickOnElement(CLOSE_MODAL_LOCATOR);
         return this;
     }
 
-    public LogInScreen clickCloseModalButton() {
-        clickOnElement(CLOSE_MODAL_LOGIN_WINDOW_BUTTON_LOCATOR);
-        return this;
+    public Boolean isWrongEmailMessageDisplayed() {
+        return WaitManager.isElementVisible(WRONG_EMAIL_MESSAGE_LOCATOR, Browser.SHORT_TIMEOUT_SECONDS);
     }
 }
