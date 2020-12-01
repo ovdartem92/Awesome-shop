466b5bb276e5f8128d88ea98fb5f518854d71d8b
Anastasiya Murashka <anastasiya_murashka@epam.com>
2020-12-01 17:49:22 +0300

    add WebDriverListener

--- src/main/java/net/skyscanner/ta/framework/listeners/WebDriverListener.java
+++ src/main/java/net/skyscanner/ta/framework/listeners/WebDriverListener.java
@@ -1,143 +1,37 @@
 package net.skyscanner.ta.framework.listeners;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.events.WebDriverEventListener;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 
-public class WebDriverListener implements WebDriverEventListener {
+public class WebDriverListener extends AbstractWebDriverEventListener {
+    private final Logger logger = LogManager.getRootLogger();
+
     @Override
-    public void beforeAlertAccept(WebDriver webDriver) {
-
+    public void beforeAlertAccept(WebDriver driver) {
+        logger.info("Before alert acceptance");
     }
 
     @Override
-    public void afterAlertAccept(WebDriver webDriver) {
-
+    public void beforeNavigateTo(String url, WebDriver driver) {
+        logger.info("Before navigate to {}", url);
     }
 
     @Override
-    public void afterAlertDismiss(WebDriver webDriver) {
-
+    public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+        logger.info("Before find by web element with locator |{}|", by);
     }
 
     @Override
-    public void beforeAlertDismiss(WebDriver webDriver) {
-
+    public void beforeClickOn(WebElement element, WebDriver driver) {
+        logger.info("Before click on element {}", element);
     }
 
     @Override
-    public void beforeNavigateTo(String s, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterNavigateTo(String s, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void beforeNavigateBack(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterNavigateBack(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void beforeNavigateForward(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterNavigateForward(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void beforeNavigateRefresh(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterNavigateRefresh(WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void beforeFindBy(By by, WebElement webElement, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterFindBy(By by, WebElement webElement, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void beforeClickOn(WebElement webElement, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterClickOn(WebElement webElement, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void beforeChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
-
-    }
-
-    @Override
-    public void afterChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
-
-    }
-
-    @Override
-    public void beforeScript(String s, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterScript(String s, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void beforeSwitchToWindow(String s, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterSwitchToWindow(String s, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void onException(Throwable throwable, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public <X> void beforeGetScreenshotAs(OutputType<X> outputType) {
-
-    }
-
-    @Override
-    public <X> void afterGetScreenshotAs(OutputType<X> outputType, X x) {
-
-    }
-
-    @Override
-    public void beforeGetText(WebElement webElement, WebDriver webDriver) {
-
-    }
-
-    @Override
-    public void afterGetText(WebElement webElement, WebDriver webDriver, String s) {
+    public void beforeChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+        logger.info("Before change value of element {} to {}", element, keysToSend);
     }
 }
