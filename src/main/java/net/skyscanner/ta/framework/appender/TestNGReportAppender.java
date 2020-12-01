7bc59e3a8467bda8107db680edc241245a96e287
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-12-01 11:13:30 +0300

    add: implement TestNGReportAppender

--- src/main/java/net/skyscanner/ta/framework/appender/TestNGReportAppender.java
+++ src/main/java/net/skyscanner/ta/framework/appender/TestNGReportAppender.java
@@ -1,4 +1,37 @@
 package net.skyscanner.ta.framework.appender;
 
-public class TestNGReportAppender {
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LoggingEvent;
+import org.testng.Reporter;
+
+public class TestNGReportAppender extends AppenderSkeleton {
+    @Override
+    protected void append(LoggingEvent loggingEvent) {
+        Reporter.log(eventToString(loggingEvent));
+    }
+
+    private String eventToString(LoggingEvent loggingEvent) {
+        final StringBuilder result = new StringBuilder(layout.format(loggingEvent));
+
+        if (layout.ignoresThrowable()) {
+            final String[] s = loggingEvent.getThrowableStrRep();
+            if (s != null) {
+                for (final String value : s) {
+                    result.append(value).append(Layout.LINE_SEP);
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
 }
