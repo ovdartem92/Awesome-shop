d71d6287d3246999f1f276e26648c9dca67028b5
Anastasiya Murashka <anastasiya_murashka@epam.com>
2020-12-04 09:24:25 +0300

    Revert "add radioButtonGroup"
    This reverts commit 2a018a2e6e454990da8d948c7d5e28c17ba9e820.

--- src/main/java/net/skyscanner/ta/framework/listeners/TestListener.java
+++ src/main/java/net/skyscanner/ta/framework/listeners/TestListener.java
@@ -1,13 +1,20 @@
 package net.skyscanner.ta.framework.listeners;
 
-import net.skyscanner.ta.framework.browser.Browser;
+import driver.Browser;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import utils.StringUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public class TestListener implements ITestListener {
@@ -27,27 +34,20 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        Objects.requireNonNull(iTestResult, "iTestResult cannot be null.");
-        logger.error("Test method {} FAILED.\n", iTestResult.getMethod().getDescription());
-        File screenshot = Browser.getInstance().takeScreenshot();
-        String screenshotTag = String.format("<a href='../../screenshots/%s'><img src='../../screenshots/%s' "
-                + "height='304' width='525'/></a>", screenshot.getName(), screenshot.getName());
-        logger.info("Screenshot {} was saved", screenshotTag);
+        logger.info("Test method {} FAILED.\n", iTestResult.getMethod().getDescription());
+        saveScreenshot(iTestResult);
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
         Objects.requireNonNull(iTestResult, "iTestResult cannot be null.");
         logger.info("Test method {} SKIPPED.\n", iTestResult.getMethod().getDescription());
-        File screenshot = Browser.getInstance().takeScreenshot();
-        String screenshotTag = String.format("<a href='../../screenshots/%s'><img src='../../screenshots/%s' "
-                + "height='304' width='525'/></a>", screenshot.getName(), screenshot.getName());
-        logger.info("Screenshot {} was saved", screenshotTag);
+        saveScreenshot(iTestResult);
     }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-        Objects.requireNonNull(iTestResult, "ITestResult cannot be null.");
+        Objects.requireNonNull(iTestResult, "ITestResult cannot be null");
     }
 
     @Override
@@ -60,5 +60,18 @@ public class TestListener implements ITestListener {
     public void onFinish(ITestContext iTestContext) {
         Objects.requireNonNull(iTestContext, "iTestContext cannot be null.");
         logger.info("Test {} FINISHED.\n", iTestContext.getName());
+    }
+
+    public void saveScreenshot(ITestResult iTestResult) {
+        File screenCapture = ((TakesScreenshot) Browser
+                .initDriver())
+                .getScreenshotAs(OutputType.FILE);
+        try {
+            Path path = Paths.get(".", "target", "screenshots", String.format("%s %s.png", iTestResult.getName(),
+                    StringUtils.getCurrentTimeAsString()));
+            FileUtils.copyFile(screenCapture, new File(path.toString()));
+        } catch (IOException e) {
+            logger.error("Failed to save screenshots {}", e.getLocalizedMessage());
+        }
     }
 }
--- src/main/java/net/skyscanner/ta/framework/ui/components/RadioButton.java
+++ src/main/java/net/skyscanner/ta/framework/ui/components/RadioButton.java
@@ -1,15 +1,11 @@
 package net.skyscanner.ta.framework.ui.components;
 
-import lombok.Getter;
-import lombok.Setter;
 import net.skyscanner.ta.framework.browser.Browser;
 import org.openqa.selenium.By;
 
 import java.util.Objects;
 
 public class RadioButton extends CommonPageElement {
-    @Getter
-    @Setter
     private RadioButtonGroup group;
 
     public RadioButton(By locator) {
