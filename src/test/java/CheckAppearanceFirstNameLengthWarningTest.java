31c64e4240127badbc1de57f3046e8f31ffb9894
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-12-14 19:58:54 +0300

    feat: add test CheckAppearanceFirstNameLengthWarningTest

---
+++ src/test/java/CheckAppearanceFirstNameLengthWarningTest.java
@@ -0,0 +1,40 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.awesome.shop.ta.product.pages.NavigatePanel;
+import ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen;
+import ru.awesome.shop.ta.utils.StringUtils;
+
+import static ru.awesome.shop.ta.product.pages.NavigatePanel.AccountLink.REGISTER;
+import static ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen.Field.FIRST_NAME;
+
+public class CheckAppearanceFirstNameLengthWarningTest extends BaseTest {
+    private NavigatePanel navigatePanel;
+    private String text = StringUtils.getRandomString();
+    private String emptyFirstName = "";
+    private String email = text.concat("@mail.ru");
+    private String region = "Bristol";
+
+    @Test
+    public void checkAppearanceFirstNameLengthWarning() {
+        navigatePanel = new NavigatePanel();
+        AccountRegistrationScreen registrationScreen = navigatePanel.openAccountLinkScreen(REGISTER)
+                .firstNameType(emptyFirstName)
+                .lastNameType(text)
+                .emailType(email)
+                .telephoneType(text)
+                .faxType(text)
+                .companyType(text)
+                .firstAddressType(text)
+                .secondAddressType(text)
+                .cityType(text)
+                .postcodeType(text)
+                .regionSelect(region)
+                .passwordType(text)
+                .passwordConfirmType(text)
+                .agreeWithPrivacyPolicyClick();
+        registrationScreen.continueButtonClick();
+
+        Assert.assertEquals(registrationScreen.getFieldWarning(FIRST_NAME),
+                "First Name must be between 1 and 32 characters!");
+    }
+}
