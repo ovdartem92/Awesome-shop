202b970cdb6f66e5368a0e7ae3894c066d429efc
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-12-14 20:51:56 +0300

    feat: add test CheckAppearanceRegionSelectedWarningTest

---
+++ src/test/java/CheckAppearanceRegionSelectedWarningTest.java
@@ -0,0 +1,37 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.awesome.shop.ta.product.pages.NavigatePanel;
+import ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen;
+import ru.awesome.shop.ta.utils.StringUtils;
+
+import static ru.awesome.shop.ta.product.pages.NavigatePanel.AccountLink.REGISTER;
+import static ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen.Field.REGION;
+
+public class CheckAppearanceRegionSelectedWarningTest extends BaseTest {
+    private NavigatePanel navigatePanel;
+    private String text = StringUtils.getRandomString();
+    private String email = text.concat("@mail.ru");
+
+    @Test
+    public void checkAppearanceFirstNameLengthWarning() {
+        navigatePanel = new NavigatePanel();
+        AccountRegistrationScreen registrationScreen = navigatePanel.openAccountLinkScreen(REGISTER)
+                .firstNameType(text)
+                .lastNameType(text)
+                .emailType(email)
+                .telephoneType(text)
+                .faxType(text)
+                .companyType(text)
+                .firstAddressType(text)
+                .secondAddressType(text)
+                .cityType(text)
+                .postcodeType(text)
+                .passwordType(text)
+                .passwordConfirmType(text)
+                .agreeWithPrivacyPolicyClick();
+        registrationScreen.continueButtonClick();
+
+        Assert.assertEquals(registrationScreen.getFieldWarning(REGION),
+                "Please select a region / state!");
+    }
+}
