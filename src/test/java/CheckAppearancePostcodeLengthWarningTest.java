ac17c829458ebf64d7f60cce9c387ef48e81a287
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-12-15 11:40:20 +0300

    feat: add test CheckAppearancePostcodeLengthWarningTest

---
+++ src/test/java/CheckAppearancePostcodeLengthWarningTest.java
@@ -0,0 +1,40 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.awesome.shop.ta.product.pages.NavigatePanel;
+import ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen;
+import ru.awesome.shop.ta.utils.StringUtils;
+
+import static ru.awesome.shop.ta.product.pages.NavigatePanel.AccountLink.REGISTER;
+import static ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen.Field.POST_CODE;
+
+public class CheckAppearancePostcodeLengthWarningTest extends BaseTest {
+    private NavigatePanel navigatePanel;
+    private String text = StringUtils.getRandomString();
+    private String emptyPostCode = "";
+    private String email = text.concat("@mail.ru");
+    private String region = "Bristol";
+
+    @Test
+    public void checkAppearancePostcodeLengthWarning() {
+        navigatePanel = new NavigatePanel();
+        AccountRegistrationScreen registrationScreen = navigatePanel.openAccountLinkScreen(REGISTER)
+                .firstNameType(text)
+                .lastNameType(text)
+                .emailType(email)
+                .telephoneType(text)
+                .faxType(text)
+                .companyType(text)
+                .firstAddressType(text)
+                .secondAddressType(text)
+                .cityType(text)
+                .postcodeType(emptyPostCode)
+                .regionSelect(region)
+                .passwordType(text)
+                .passwordConfirmType(text)
+                .agreeWithPrivacyPolicyClick();
+        registrationScreen.continueButtonClick();
+
+        Assert.assertEquals(registrationScreen.getFieldWarning(POST_CODE),
+                "Postcode must be between 2 and 10 characters!");
+    }
+}
