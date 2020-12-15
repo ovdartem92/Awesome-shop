e4d1df4460906d3fe85adf7a688e251e01876554
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-12-15 16:35:07 +0300

    feat: add CheckAppearancePrivatePolicyWindowTest

---
+++ src/test/java/CheckAppearancePrivatePolicyWindowTest.java
@@ -0,0 +1,19 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.awesome.shop.ta.product.pages.NavigatePanel;
+import ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen;
+
+import static ru.awesome.shop.ta.product.pages.NavigatePanel.AccountLink.REGISTER;
+
+public class CheckAppearancePrivatePolicyWindowTest extends BaseTest {
+    private NavigatePanel navigatePanel;
+
+    @Test
+    public void checkAppearancePrivatePolicyWindow() {
+        navigatePanel = new NavigatePanel();
+        AccountRegistrationScreen registrationScreen = navigatePanel.openAccountLinkScreen(REGISTER)
+                .privacyPolicyClick();
+
+        Assert.assertTrue(registrationScreen.isDisplayedPrivacyPolicyWindow());
+    }
+}
