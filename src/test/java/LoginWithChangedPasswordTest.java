1a261390516202fe9ee55ea7aef6774a5a0f5573
Anastasiya Murashka <anastasiya_murashka@epam.com>
2020-12-15 22:20:20 +0300

    Update LoginWithChangedPasswordTest.java

--- src/test/java/LoginWithChangedPasswordTest.java
+++ src/test/java/LoginWithChangedPasswordTest.java
@@ -1,18 +1,18 @@
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import ru.awesome.shop.ta.framework.services.UserBuilder;
 import ru.awesome.shop.ta.product.bo.user.User;
+import ru.awesome.shop.ta.product.pages.AccountPage;
 import ru.awesome.shop.ta.product.pages.Header;
-import ru.awesome.shop.ta.utils.TestDataReader;
-
-import java.io.IOException;
 
 import static ru.awesome.shop.ta.utils.StringUtils.getRandomString;
 
 public class LoginWithChangedPasswordTest extends BaseTest {
+    private User user = UserBuilder.getUserWithValidCredentials();
+
     @Test(description = "Check that user can login after changing password")
-    public void loginWithChangedPassword() throws IOException {
-        User user = UserBuilder.getUserWithValidCredentials();
+    public void loginWithChangedPassword() {
         String newPassword = getRandomString();
         new Header().clickOnMyAccountLink().clickOnLoginLink()
                 .typeEmailAddress(user.getEmail())
@@ -23,5 +23,11 @@ public class LoginWithChangedPasswordTest extends BaseTest {
         boolean accountLabel = new Header().clickOnMyAccountLink().clickOnLoginLink().typeEmailAddress(user.getEmail())
                 .typePassword(newPassword).clickOnLoginButton().isTextFromMyAccountLabelDisplayed();
         Assert.assertTrue(accountLabel, "User can not login after successfully changing password");
+    }
+
+    @AfterClass(description = "change password back")
+    public void changePasswordBack() {
+        new AccountPage().clickOnChangePasswordLink().typeToPasswordTextField(user.getPassword())
+                .typeToConfirmPasswordTextField(user.getPassword()).clickOnContinueButton();
     }
 }
