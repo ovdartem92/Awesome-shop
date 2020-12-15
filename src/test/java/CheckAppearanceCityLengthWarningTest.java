2f5f4d15ae1c71f58bd9fea14f0f0515a93e4602
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-12-15 11:57:15 +0300

    feat: add test CheckAppearanceCityLengthWarningTest

---
+++ src/test/java/CheckAppearanceCityLengthWarningTest.java
@@ -0,0 +1,40 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.awesome.shop.ta.product.pages.NavigatePanel;
+import ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen;
+import ru.awesome.shop.ta.utils.StringUtils;
+
+import static ru.awesome.shop.ta.product.pages.NavigatePanel.AccountLink.REGISTER;
+import static ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen.Field.CITY;
+
+public class CheckAppearanceCityLengthWarningTest extends BaseTest {
+    private NavigatePanel navigatePanel;
+    private String text = StringUtils.getRandomString();
+    private String emptyCity = "";
+    private String email = text.concat("@mail.ru");
+    private String region = "Bristol";
+
+    @Test
+    public void checkAppearanceFirstAddressLengthWarning() {
+        navigatePanel = new NavigatePanel();
+        AccountRegistrationScreen registrationScreen = navigatePanel.openAccountLinkScreen(REGISTER)
+                .firstNameType(text)
+                .lastNameType(text)
+                .emailType(email)
+                .telephoneType(text)
+                .faxType(text)
+                .companyType(text)
+                .firstAddressType(text)
+                .secondAddressType(text)
+                .cityType(emptyCity)
+                .postcodeType(text)
+                .regionSelect(region)
+                .passwordType(text)
+                .passwordConfirmType(text)
+                .agreeWithPrivacyPolicyClick();
+        registrationScreen.continueButtonClick();
+
+        Assert.assertEquals(registrationScreen.getFieldWarning(CITY),
+                "City must be between 2 and 128 characters!");
+    }
+}
