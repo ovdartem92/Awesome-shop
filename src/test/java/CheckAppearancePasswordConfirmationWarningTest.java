d2079f008c68a859c5c40624374ff95eebbcdf06
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-12-15 11:57:34 +0300

    feat: add test CheckAppearancePasswordConfirmationWarningTest

---
+++ src/test/java/CheckAppearancePasswordConfirmationWarningTest.java
@@ -0,0 +1,40 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.awesome.shop.ta.product.pages.NavigatePanel;
+import ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen;
+import ru.awesome.shop.ta.utils.StringUtils;
+
+import static ru.awesome.shop.ta.product.pages.NavigatePanel.AccountLink.REGISTER;
+import static ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen.Field.PASSWORD_CONFIRM;
+
+public class CheckAppearancePasswordConfirmationWarningTest extends BaseTest {
+    private NavigatePanel navigatePanel;
+    private String text = StringUtils.getRandomString();
+    private String emptyPasswordConfirm = "";
+    private String email = text.concat("@mail.ru");
+    private String region = "Bristol";
+
+    @Test
+    public void checkAppearancePasswordConfirmationWarning() {
+        navigatePanel = new NavigatePanel();
+        AccountRegistrationScreen registrationScreen = navigatePanel.openAccountLinkScreen(REGISTER)
+                .firstNameType(text)
+                .lastNameType(text)
+                .emailType(email)
+                .telephoneType(text)
+                .faxType(text)
+                .companyType(text)
+                .firstAddressType(text)
+                .secondAddressType(text)
+                .cityType(text)
+                .postcodeType(text)
+                .regionSelect(region)
+                .passwordType(text)
+                .passwordConfirmType(emptyPasswordConfirm)
+                .agreeWithPrivacyPolicyClick();
+        registrationScreen.continueButtonClick();
+
+        Assert.assertEquals(registrationScreen.getFieldWarning(PASSWORD_CONFIRM),
+                "Password confirmation does not match password!");
+    }
+}
