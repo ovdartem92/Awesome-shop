9877624a0d73dddb36975e8f071e910ec52c8bda
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-12-15 11:39:49 +0300

    feat: add test CheckAppearanceEmailInvalidWarningTest

---
+++ src/test/java/CheckAppearanceEmailInvalidWarningTest.java
@@ -0,0 +1,45 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.awesome.shop.ta.product.pages.NavigatePanel;
+import ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen;
+import ru.awesome.shop.ta.utils.StringUtils;
+
+import static ru.awesome.shop.ta.product.pages.NavigatePanel.AccountLink.REGISTER;
+import static ru.awesome.shop.ta.product.pages.registration.AccountRegistrationScreen.Field.EMAIL;
+
+public class CheckAppearanceEmailInvalidWarningTest extends BaseTest {
+    private NavigatePanel navigatePanel;
+    private String text = StringUtils.getRandomString();
+    private String emailWithoutSymbol = text.concat("mail.ru");
+    private String region = "Bristol";
+
+    @Test
+    public void checkAppearanceEmailInvalidWarning() {
+        navigatePanel = new NavigatePanel();
+        AccountRegistrationScreen registrationScreen = navigatePanel.openAccountLinkScreen(REGISTER)
+                .firstNameType(text)
+                .lastNameType(text)
+                .emailType(emailWithoutSymbol)
+                .telephoneType(text)
+                .faxType(text)
+                .companyType(text)
+                .firstAddressType(text)
+                .secondAddressType(text)
+                .cityType(text)
+                .postcodeType(text)
+                .regionSelect(region)
+                .passwordType(text)
+                .passwordConfirmType(text)
+                .agreeWithPrivacyPolicyClick();
+        registrationScreen.continueButtonClick();
+
+        try {
+            Thread.sleep(20000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(registrationScreen.getFieldWarning(EMAIL),
+                "E-Mail Address does not appear to be valid!");
+    }
+}
