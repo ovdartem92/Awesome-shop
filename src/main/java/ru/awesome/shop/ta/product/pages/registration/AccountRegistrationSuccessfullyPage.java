93dd0aa8830ba207449dc584e62853d6d8c34402
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-12-17 14:45:55 +0300

    fix: refactor AccountRegistrationSuccessfullyPage class

--- src/main/java/ru/awesome/shop/ta/product/pages/registration/AccountRegistrationSuccessfullyPage.java
+++ src/main/java/ru/awesome/shop/ta/product/pages/registration/AccountRegistrationSuccessfullyPage.java
@@ -4,15 +4,15 @@ import org.openqa.selenium.By;
 import ru.awesome.shop.ta.framework.ui.components.Button;
 import ru.awesome.shop.ta.framework.ui.components.Label;
 
-public class AccountRegistrationSuccessfullyScreen {
-    private Label accountCreatedLabel = new Label(By.tagName("h1"));
+public class AccountRegistrationSuccessfullyPage {
+    private Label accountCreationMessage = new Label(By.tagName("h1"));
     private Button continueButton = new Button(By.className("pull-right"));
 
-    public String getAccountCreatedLabelText() {
-        return accountCreatedLabel.getText();
+    public String getAccountCreationMessage() {
+        return accountCreationMessage.getText();
     }
 
-    public AccountRegistrationSuccessfullyScreen clickContinueButton() {
+    public AccountRegistrationSuccessfullyPage clickContinueButton() {
         continueButton.click();
         return this;
     }
