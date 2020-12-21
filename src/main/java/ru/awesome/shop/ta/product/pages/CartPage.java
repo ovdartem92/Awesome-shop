9fa5c65d2bb9929d1ea8a566d346f71314f32f89
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-12-21 20:08:38 +0300

    add: new methods to HomePage and CartPage

--- src/main/java/ru/awesome/shop/ta/product/pages/CartPage.java
+++ src/main/java/ru/awesome/shop/ta/product/pages/CartPage.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.WebElement;
 import ru.awesome.shop.ta.framework.browser.Browser;
 import ru.awesome.shop.ta.framework.ui.components.Button;
 import ru.awesome.shop.ta.framework.ui.components.Label;
+import ru.awesome.shop.ta.framework.ui.components.TextField;
 import ru.awesome.shop.ta.product.pages.fragments.CartItemFragment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static ru.awesome.shop.ta.framework.ui.components.CommonPageElement.waitForAllElementsPresenceLocated;
 import static ru.awesome.shop.ta.framework.ui.components.CommonPageElement.waitForPageElementInvisibilityLocated;
@@ -66,12 +68,58 @@ public class CartPage extends BasePage {
         return warningQuantityLabel.getText();
     }
 
-    public void deleteItemFromCartByName(String productName) {
+    public void clickRemoveItemFromCartByName(String productName) {
         By removeButtonLocator = By.xpath(String.format(
                 "//div[@class='table-responsive']//a[text()='%s']/ancestor::" +
                         "tr//button[contains(@onclick,'cart.remove')]", productName));
         Button removeButton = new Button(removeButtonLocator);
         removeButton.click();
         waitForPageElementInvisibilityLocated(removeButtonLocator, 3);
+    }
+
+    public String getItemNameByName(String productName) {
+        By nameLabelLocator = By.xpath(String.format("//div[@class='table-responsive']//a[text()='%s']", productName));
+        Label itemNameLabel = new Label(nameLabelLocator);
+        return itemNameLabel.getText();
+    }
+
+    public String getItemUnitPriceByName(String productName) {
+        By unitPriceLabelLocator = By.xpath(String.format(
+                "//div[@class='table-responsive']//a[text()='%s']/ancestor::tr//td[5]", productName));
+        Label unitPriceLabel = new Label(unitPriceLabelLocator);
+        return unitPriceLabel.getText();
+    }
+
+    public void clickUpdateItemButtonByName(String productName) {
+        By updateItemButtonLocator = By.xpath(String.format(
+                "//div[@class='table-responsive']//a[text()='%s']/ancestor::tr//button[@data-original-title='Update']",
+                productName));
+        Button updateItemButton = new Button(updateItemButtonLocator);
+        updateItemButton.click();
+    }
+
+    public void typeItemQuantityByName(String productName, int quantity) {
+        Objects.requireNonNull(quantity, "QUANTITY cannot be null.");
+        By quantityFieldLocator = By.xpath(String.format(
+                "//div[@class='table-responsive']//a[text()='%s']/ancestor::tr//input[contains(@name,'quantity')]",
+                productName));
+        TextField quantityField = new TextField(quantityFieldLocator);
+        quantityField.clear();
+        quantityField.type(String.valueOf(quantity));
+    }
+
+    public int getItemQuantityValueByName(String productName) {
+        By quantityFieldLocator = By.xpath(String.format(
+                "//div[@class='table-responsive']//a[text()='%s']/ancestor::tr//input[contains(@name,'quantity')]",
+                productName));
+        TextField quantityField = new TextField(quantityFieldLocator);
+        return Integer.parseInt(quantityField.getAttribute("value"));
+    }
+
+    public int getSizeOfCartItemsList() {
+        By cartItemLocator = By.xpath("//div[@class='table-responsive']//tbody//tr");
+        waitForAllElementsPresenceLocated(cartItemLocator);
+        List<WebElement> cartItemElements = Browser.getInstance().getWrappedDriver().findElements(cartItemLocator);
+        return cartItemElements.size();
     }
 }
--- src/main/java/ru/awesome/shop/ta/product/pages/HomePage.java
+++ src/main/java/ru/awesome/shop/ta/product/pages/HomePage.java
@@ -3,6 +3,9 @@ package ru.awesome.shop.ta.product.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import ru.awesome.shop.ta.framework.browser.Browser;
+import ru.awesome.shop.ta.framework.ui.components.Button;
+import ru.awesome.shop.ta.framework.ui.components.Label;
+import ru.awesome.shop.ta.framework.ui.components.Link;
 import ru.awesome.shop.ta.product.pages.fragments.HomeProductFragment;
 
 import java.util.ArrayList;
@@ -27,6 +30,30 @@ public class HomePage extends BasePage {
     public HomePage open() {
         Browser.getInstance().navigate(BASE_URL.concat(HOME_PAGE_URL));
         return this;
+    }
+
+    public void clickAddProductToCartButtonByName(String productName) {
+        By addToCartButtonLocator = By
+                .xpath(String.format("//div[@class='product-thumb transition']//a[text()='%s']/ancestor::" +
+                        "div[@class='product-thumb transition']//button[contains(@onclick,'cart.add')]", productName));
+        Button addToCartButton = new Button(addToCartButtonLocator);
+        addToCartButton.click();
+    }
+
+    public String getProductNameByName(String productName) {
+        By productNameLinkLocator = By
+                .xpath(String.format("//div[@class='product-thumb transition']//a[text()='%s']", productName));
+        Link productNameLink = new Link(productNameLinkLocator);
+        return productNameLink.getText();
+    }
+
+    public String getProductPriceByName(String productName) {
+        By productPriceLabelLocator = By
+                .xpath(String.format("//div[@class='product-thumb transition']//a[text()='%s']/ancestor::" +
+                        "div[@class='product-thumb transition']//p[@class='price']", productName));
+        Label productPriceLabel = new Label(productPriceLabelLocator);
+        String[] array = productPriceLabel.getText().split("\n");
+        return array[0];
     }
 }
 
