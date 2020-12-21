55f54e24b2262b8ad1f6a8f31347b96febb304a8
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-12-21 20:26:47 +0300

    fix: clickAddProductToCartButtonByName() add check CartTotalButton locator

--- src/main/java/ru/awesome/shop/ta/product/pages/HomePage.java
+++ src/main/java/ru/awesome/shop/ta/product/pages/HomePage.java
@@ -6,6 +6,8 @@ import ru.awesome.shop.ta.framework.ui.components.Button;
 import ru.awesome.shop.ta.framework.ui.components.Label;
 import ru.awesome.shop.ta.framework.ui.components.Link;
 
+import static ru.awesome.shop.ta.framework.ui.components.CommonPageElement.waitForPageElementVisibilityLocated;
+
 public class HomePage extends BasePage {
     private static final String HOME_PAGE_URL = "index.php?route=common/home";
 
@@ -15,11 +17,19 @@ public class HomePage extends BasePage {
     }
 
     public void clickAddProductToCartButtonByName(String productName) {
+        Button cartTotalButton = new Button(By.xpath("//div[@id='cart']"));
+        String startTextFromCartTotalButton = cartTotalButton.getText();
+        String[] arrayButtonInfo = startTextFromCartTotalButton.split(" ");
+        int startQuantity = Integer.parseInt(arrayButtonInfo[0]);
+        String endQuantity = Integer.toString(startQuantity + 1);
+        String expectTextFromCartTotalButton = String
+                .format("//span[@id='cart-total'][contains(text(), '%s item(s)')]", endQuantity);
         By addToCartButtonLocator = By
                 .xpath(String.format("//div[@class='product-thumb transition']//a[text()='%s']/ancestor::" +
                         "div[@class='product-thumb transition']//button[contains(@onclick,'cart.add')]", productName));
         Button addToCartButton = new Button(addToCartButtonLocator);
         addToCartButton.click();
+        waitForPageElementVisibilityLocated(By.xpath(expectTextFromCartTotalButton), 3);
     }
 
     public String getProductNameByName(String productName) {
