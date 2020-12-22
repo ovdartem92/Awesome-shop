ecea7bbc74ad4ac993b3fd5b2ee601bc00678a4d
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2020-12-22 23:17:59 +0300

    fix: add changes in BaseConfigurationTest

--- src/test/java/awesome/shop/tests/BaseConfigurationTest.java
+++ src/test/java/awesome/shop/tests/BaseConfigurationTest.java
@@ -1,9 +1,13 @@
 package awesome.shop.tests;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Listeners;
 import ru.awesome.shop.ta.framework.browser.Browser;
+import ru.awesome.shop.ta.framework.listeners.SuiteListener;
+import ru.awesome.shop.ta.framework.listeners.TestListener;
 
+@Listeners({TestListener.class, SuiteListener.class})
 public abstract class BaseConfigurationTest {
     private Browser browser;
 
