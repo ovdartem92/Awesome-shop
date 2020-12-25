e1ba00457977f4f487f38a7d37730a0a5dc9f6fe
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-12-25 18:26:59 +0300

    add: add minor changes

--- src/test/java/awesome/shop/tests/BaseConfigurationTest.java
+++ src/test/java/awesome/shop/tests/BaseConfigurationTest.java
@@ -1,7 +1,7 @@
 package awesome.shop.tests;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Listeners;
 import ru.awesome.shop.ta.framework.browser.Browser;
 import ru.awesome.shop.ta.framework.listeners.SuiteListener;
