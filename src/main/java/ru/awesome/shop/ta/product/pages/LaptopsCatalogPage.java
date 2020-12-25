c724dfac99269baf3b316718c258228d26e685d3
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2020-12-25 23:24:14 +0300

    add: getName() and getPrice() to Catalog pages

--- src/main/java/ru/awesome/shop/ta/product/pages/LaptopsCatalogPage.java
+++ src/main/java/ru/awesome/shop/ta/product/pages/LaptopsCatalogPage.java
@@ -3,6 +3,8 @@ package ru.awesome.shop.ta.product.pages;
 import org.openqa.selenium.By;
 import ru.awesome.shop.ta.framework.browser.Browser;
 import ru.awesome.shop.ta.framework.ui.components.Button;
+import ru.awesome.shop.ta.framework.ui.components.Label;
+import ru.awesome.shop.ta.framework.ui.components.Link;
 
 public class LaptopsCatalogPage extends BasePage {
     private static final String LAPTOPS_CATALOG_PAGE_URL = "index.php?route=product/category&path=18";
@@ -18,5 +20,21 @@ public class LaptopsCatalogPage extends BasePage {
                         "::div[@class='product-thumb']//button[contains(@onclick,'cart.add')]", laptopName));
         Button addToCartButton = new Button(addToCartButtonLocator);
         addToCartButton.click();
+    }
+
+    public String getLaptopName(String laptopName) {
+        By laptopNameLinkLocator = By
+                .xpath(String.format("//div[@class='product-thumb']//a[text()='%s']", laptopName));
+        Link laptopNameLink = new Link(laptopNameLinkLocator);
+        return laptopNameLink.getText();
+    }
+
+    public String getLaptopPrice(String laptopPrice) {
+        By laptopPriceLabelLocator = By
+                .xpath(String.format("//div[@class='product-thumb']//a[text()='%s']/ancestor::" +
+                        "div[@class='product-thumb']//p[@class='price']", laptopPrice));
+        Label laptopPriceLabel = new Label(laptopPriceLabelLocator);
+        String[] array = laptopPriceLabel.getText().split("\n");
+        return array[0];
     }
 }
--- src/main/java/ru/awesome/shop/ta/product/pages/PhonesCatalogPage.java
+++ src/main/java/ru/awesome/shop/ta/product/pages/PhonesCatalogPage.java
@@ -3,6 +3,8 @@ package ru.awesome.shop.ta.product.pages;
 import org.openqa.selenium.By;
 import ru.awesome.shop.ta.framework.browser.Browser;
 import ru.awesome.shop.ta.framework.ui.components.Button;
+import ru.awesome.shop.ta.framework.ui.components.Label;
+import ru.awesome.shop.ta.framework.ui.components.Link;
 
 public class PhonesCatalogPage extends BasePage {
     private static final String PHONES_CATALOG_PAGE_URL = "index.php?route=product/category&path=24";
@@ -18,5 +20,21 @@ public class PhonesCatalogPage extends BasePage {
                         "::div[@class='product-thumb']//button[contains(@onclick,'cart.add')]", phoneName));
         Button addToCartButton = new Button(addToCartButtonLocator);
         addToCartButton.click();
+    }
+
+    public String getPhoneName(String phoneName) {
+        By phoneNameLinkLocator = By
+                .xpath(String.format("//div[@class='product-thumb']//a[text()='%s']", phoneName));
+        Link phoneNameLink = new Link(phoneNameLinkLocator);
+        return phoneNameLink.getText();
+    }
+
+    public String getPhonePrice(String phonePrice) {
+        By phonePriceLabelLocator = By
+                .xpath(String.format("//div[@class='product-thumb']//a[text()='%s']/ancestor::" +
+                        "div[@class='product-thumb']//p[@class='price']", phonePrice));
+        Label phonePriceLabel = new Label(phonePriceLabelLocator);
+        String[] array = phonePriceLabel.getText().split("\n");
+        return array[0];
     }
 }
