9fcb57d19a4e932abcc347d662e2452b87b5fef3
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2020-12-29 14:20:19 +0300

    add: add minor changes

--- src/main/java/ru/awesome/shop/ta/product/pages/fragments/SearchFragment.java
+++ src/main/java/ru/awesome/shop/ta/product/pages/fragments/SearchFragment.java
@@ -1,20 +1,21 @@
 package ru.awesome.shop.ta.product.pages.fragments;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import ru.awesome.shop.ta.framework.ui.components.Button;
 import ru.awesome.shop.ta.framework.ui.components.TextField;
 import ru.awesome.shop.ta.product.pages.SearchResultPage;
 
 public class SearchFragment {
+    private final By searchTextInputLocator = By.xpath("//*[@id='search']//input");
 
     public SearchResultPage performSearchByPressingEnter(String searchCriteria) {
-        String enter = "\n";
-        typeSearchQuery(searchCriteria + enter);
+        TextField searchTextField = new TextField(searchTextInputLocator);
+        searchTextField.type(searchCriteria + Keys.ENTER);
         return new SearchResultPage();
     }
 
     public SearchFragment typeSearchQuery(String searchCriteria) {
-        By searchTextInputLocator = By.xpath("//*[@id='search']//input");
         TextField textField = new TextField(searchTextInputLocator);
         textField.clear();
         textField.type(searchCriteria);
