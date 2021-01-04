32fd4a6c525b99c1ac95239867017b4f9277e64c
Kiryl_Kebikau <Kiryl_Kebikau@epam.com>
2021-01-05 02:26:53 +0300

    fix: change registration scenario

--- src/test/java/awesome/shop/tests/cucumber/steps/registration/RegistrationSteps.java
+++ src/test/java/awesome/shop/tests/cucumber/steps/registration/RegistrationSteps.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.testng.Assert;
+import ru.awesome.shop.ta.framework.configuration.PropertyManager;
 import ru.awesome.shop.ta.framework.exceptions.RegistrationException;
 import ru.awesome.shop.ta.product.bo.address.Address;
 import ru.awesome.shop.ta.product.bo.address.Region;
@@ -22,6 +23,8 @@ import java.util.Map;
 import java.util.Objects;
 
 public class RegistrationSteps {
+    private final String REGISTERED_EMAIL = "alexandr_ladygin@mail.ru";
+    private final String REGISTERED_PASSWORD = "qwerty12345";
     private final AccountRegistrationService accountRegistrationService = new AccountRegistrationService();
     private final AccountRegistrationPage accountRegistrationPage = new AccountRegistrationPage();
     private final SuccessfulAccountRegistrationPage successfulRegistrationPage = new SuccessfulAccountRegistrationPage();
@@ -48,11 +51,10 @@ public class RegistrationSteps {
         Region region = validAddress.getRegion();
 
         for (Map<String, String> form : signUpForm) {
-            System.out.println(form);
+            String email;
+            String password;
             String firstName = Objects.requireNonNullElse(form.get("First Name"), validUser.getFirstName());
             String lastName = Objects.requireNonNullElse(form.get("Last Name"), validUser.getLastName());
-            String email = Objects.requireNonNullElse(form.get("Email"), validCredentials.getEmail());
-            String password = Objects.requireNonNullElse(form.get("Password"), validCredentials.getPassword());
             String telephone = Objects.requireNonNullElse(form.get("Telephone"), validContactInfo.getTelephoneNumber());
             String fax = Objects.requireNonNullElse(form.get("Fax"), validContactInfo.getFaxNumber());
             String company = Objects.requireNonNullElse(form.get("Company"), validUser.getCompanyName());
@@ -61,13 +63,18 @@ public class RegistrationSteps {
             String city = Objects.requireNonNullElse(form.get("City"), validAddress.getCity());
             String postCode = Objects.requireNonNullElse(form.get("Postcode"), validAddress.getPostCode());
 
+            if (REGISTERED_EMAIL.equals(form.get("Email")) || REGISTERED_PASSWORD.equals(form.get("Password"))) {
+                email = PropertyManager.getEmail();
+                password = PropertyManager.getPassword();
+            } else {
+                email = Objects.requireNonNullElse(form.get("Email"), validCredentials.getEmail());
+                password = Objects.requireNonNullElse(form.get("Password"), validCredentials.getPassword());
+            }
             Credentials credentials = new Credentials(email, password);
             Address address = new Address(firstAddress, secondAddress, city, postCode, region);
             ContactInfo contactInfo = new ContactInfo(telephone, fax, address);
             user = new User.Builder(credentials).firstName(firstName).lastName(lastName)
                     .companyName(company).contactInfo(contactInfo).build();
-            System.out.println("FIRST_NAME: " + firstName);
-            System.out.println("LAST_NAME: " + lastName);
         }
 
         try {
@@ -81,21 +88,21 @@ public class RegistrationSteps {
 
     @When("^I register with empty data$")
     public void registerWithEmptyData() {
+        String email = "";
+        String password = "";
+        Credentials credentials = new Credentials(email, password);
+        user = new User.Builder(credentials).build();
         try {
-            String email = "";
-            String password = "";
-            Credentials credentials = new Credentials(email, password);
-            user = new User.Builder(credentials).build();
             accountRegistrationService.register(user);
         } catch (RegistrationException ex) {
             String errorMessage = ex.getMessage();
             String[] split = errorMessage.split(":");
-            testContext.setErrorMessage(split[2].trim());
+            testContext.setErrorMessage(split[0]);
         }
     }
 
-    @When("^I am registering as a user with the consent to the license agreement is \"([^\"]*)\"$")
-    public void registerAsValidUser_for_a_license_agreement(String isAgreeWithLicense) {
+    @When("^I am registering as a user with the consent to the license agreement is \"(.*)\"$")
+    public void registerWithValidData(String isAgreeWithLicense) {
         boolean isSubscribed = false;
         boolean isAgree = isAgreeWithLicense.equals("true");
         user = UserFactory.generateValidUser();
@@ -113,16 +120,16 @@ public class RegistrationSteps {
         accountRegistrationPage.clickPrivacyPolicyLink();
     }
 
-    @Then("^I should see registration error message \"([^\"]*)\"$")
+    @Then("^I should see registration error message \"(.*)\"$")
     public void checkRegistrationErrorMessage(String expectedErrorMessage) {
         String errorMessage = testContext.getErrorMessage();
         Assert.assertEquals(errorMessage, expectedErrorMessage,
                 "The expected error message does not match the actual one");
     }
 
-    @Then("^I should see \"([^\"]*)\" pop up window$")
-    public void checkPopUpWindowTitle(String title) {
-        Assert.assertEquals(title, "Privacy Policy", "Incorrect window title");
+    @Then("^I should see \"(.*)\" pop up window$")
+    public void checkPopUpWindowTitle(String expectedTitle) {
+        Assert.assertEquals( "Privacy Policy", expectedTitle, "Incorrect pop up window title");
     }
 
     @Then("^I should see home page$")
--- src/test/resources/features/Registration.feature
+++ src/test/resources/features/Registration.feature
@@ -6,44 +6,43 @@ Feature: Registration Functionality Feature
   Background: open registration page
     Given I have opened the registration page
 
-#  @test @registration @positive
-#  Scenario: Registration with empty user parameters
-#    When I register with empty data
-#    Then I should see registration error message "Registration failed"
-#
-#  @test @registration @negative
-#  Scenario Outline: Registration with one invalid user parameter
-#    When I register with data:
-#      | First Name   | Last Name   | City   | Telephone   |
-#      | <first_name> | <last_name> | <city> | <telephone> |
-#    Then I should see registration error message "Registration failed"
-#
-#    Examples:
-#      | first_name | last_name | city    | telephone    |
-#      | Joe#       | Dow       | London  | +1234567890  |
-#      | Joe        | Dow$      | London  | +1234567890  |
-#      | Joe        | Dow       | L@ondon | +1234567890  |
-#      | Joe        | Dow       | London  | +"1234567890 |
-#
-#  @test @registration @positive @smoke
-#  Scenario: Registration  as a valid user
-#    When I am registering as a user with the consent to the license agreement is "false"
-#    Then I should see home page
+  @test @registration @positive
+  Scenario: Registration with empty user parameters
+    When I register with empty data
+    Then I should see registration error message "Registration failed"
+
+  @test @registration @negative
+  Scenario Outline: Registration with one invalid user parameter
+    When I register with data:
+      | First Name   | Last Name   | City   | Telephone   |
+      | <first_name> | <last_name> | <city> | <telephone> |
+    Then I should see registration error message "Registration failed"
+
+    Examples:
+      | first_name | last_name | city    | telephone    |
+      | Joe#       | Dow       | London  | +1234567890  |
+      | Joe        | Dow$      | London  | +1234567890  |
+      | Joe        | Dow       | L@ondon | +1234567890  |
+      | Joe        | Dow       | London  | +"1234567890 |
+
+  @test @registration @positive @smoke
+  Scenario: Registration as a valid user
+    When I am registering as a user with the consent to the license agreement is "true"
+    Then I should see home page
 
   @test @registration @positive
   Scenario: Registration with an already registered email
     When I register with data:
-      | Email             | Password             |
-      | $REGISTERED_EMAIL | $REGISTERED_PASSWORD |
-    Then I should see registration error message "E-Mail Address is already registered!"
+      | Email                    | Password    |
+      | alexandr_ladygin@mail.ru | qwerty12345 |
+    Then I should see registration error message "Registration failed"
 
-#  @test @registration @positive
-#  Scenario: Registration without consent to the license agreement
-#    When I am registering as a user with the consent to the license agreement is "false"
-#    Then I should see registration error message "You must agree to the Privacy Policy!"
-#
-#  @test @registration @positive
-#  Scenario: Open the license agreement window with the corresponding title
-#
-#    When I click privacy policy link
-#    Then I should see "Privacy Policy" pop up window
+  @test @registration @positive
+  Scenario: Registration without consent to the license agreement
+    When I am registering as a user with the consent to the license agreement is "false"
+    Then I should see registration error message "You must agree to the Privacy Policy!"
+
+  @test @registration @positive
+  Scenario: Open the license agreement window with the corresponding title
+    When I click privacy policy link
+    Then I should see "Privacy Policy" pop up window
