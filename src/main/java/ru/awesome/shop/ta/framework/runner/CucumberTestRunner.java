bf419f66dc01f6cbdbed36c2897db6dc2f505e41
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2021-01-04 19:17:02 +0300

    fix: delete TestListener from CucumberTestRunner, add saving screens to hooks after method

--- src/main/java/ru/awesome/shop/ta/framework/runner/CucumberTestRunner.java
+++ src/main/java/ru/awesome/shop/ta/framework/runner/CucumberTestRunner.java
@@ -4,7 +4,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Listeners;
 import ru.awesome.shop.ta.framework.listeners.SuiteListener;
-import ru.awesome.shop.ta.framework.listeners.TestListener;
 
 @CucumberOptions(
         plugin = {"pretty",
@@ -12,9 +11,7 @@ import ru.awesome.shop.ta.framework.listeners.TestListener;
         monochrome = true,
         tags = "@smoke",
         glue = "awesome.shop.tests.cucumber",
-        features = "src/test/resources/features"
-
-)
-@Listeners({TestListener.class, SuiteListener.class})
+        features = "src/test/resources/features")
+@Listeners({SuiteListener.class})
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
 }
--- src/test/java/awesome/shop/tests/cucumber/hooks/BrowserHooks.java
+++ src/test/java/awesome/shop/tests/cucumber/hooks/BrowserHooks.java
@@ -2,8 +2,15 @@ package awesome.shop.tests.cucumber.hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import ru.awesome.shop.ta.framework.browser.Browser;
 import ru.awesome.shop.ta.framework.configuration.PropertyManager;
+import ru.awesome.shop.ta.framework.logging.Log;
+
+import java.io.File;
+import java.util.Objects;
 
 public class BrowserHooks {
     protected Browser browser;
@@ -15,7 +22,16 @@ public class BrowserHooks {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        Objects.requireNonNull(scenario, "iTestResult cannot be null.");
+        if (scenario.isFailed()) {
+            File screenshot = browser.takeScreenshot();
+            String screenshotTag = String.format("<a href='../../screenshots/%s'><img src='../../screenshots/%s' "
+                    + "height='304' width='525'/></a>", screenshot.getName(), screenshot.getName());
+            Log.info(String.format("Screenshot %s was saved", screenshotTag));
+            byte[] screenshotForReport = ((TakesScreenshot) browser.getWrappedDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshotForReport, "image/png", scenario.getName());
+        }
         browser.stop();
     }
 }
