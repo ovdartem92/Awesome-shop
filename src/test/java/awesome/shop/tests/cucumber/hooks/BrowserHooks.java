135f212f2583484d3ef4999d894bd7556b7b1ac5
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2021-01-05 20:05:57 +0300

    feature: extend tearDown() - add saving screenshots to target and reporter html

--- src/test/java/awesome/shop/tests/cucumber/hooks/BrowserHooks.java
+++ src/test/java/awesome/shop/tests/cucumber/hooks/BrowserHooks.java
@@ -2,8 +2,14 @@ package awesome.shop.tests.cucumber.hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
 import ru.awesome.shop.ta.framework.browser.Browser;
 import ru.awesome.shop.ta.framework.configuration.PropertyManager;
+import ru.awesome.shop.ta.framework.logging.Log;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BrowserHooks {
     protected Browser browser;
@@ -15,7 +21,15 @@ public class BrowserHooks {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) throws IOException {
+        if (scenario.isFailed()) {
+            Log.info(String.format("SCENARIO named: %s is FAILED!", scenario.getName()));
+            File screenshot = browser.takeScreenshot();
+            Log.info("SCREENSHOT was saved to: target/screenshots");
+            byte[] screenshotByteArray = FileUtils.readFileToByteArray(screenshot);
+            scenario.attach(screenshotByteArray, "image/png", scenario.getName());
+            Log.info("SCREENSHOT was attached to: target/cucumber-reports/cucumber-pretty.html");
+        }
         browser.stop();
     }
 }
