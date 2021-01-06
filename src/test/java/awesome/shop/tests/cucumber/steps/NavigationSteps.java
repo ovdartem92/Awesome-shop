2bcf7773396a747d4583874f5385a097bf8b4ce9
Anastasiya Murashka <anastasiya_murashka@epam.com>
2021-01-06 15:25:59 +0300

    Update NavigationSteps.java

--- src/test/java/awesome/shop/tests/cucumber/steps/NavigationSteps.java
+++ src/test/java/awesome/shop/tests/cucumber/steps/NavigationSteps.java
@@ -4,10 +4,10 @@ import io.cucumber.java.en.Given;
 import ru.awesome.shop.ta.product.services.NavigationService;
 
 public class NavigationSteps {
+    private NavigationService navigationService = new NavigationService();
 
     @Given("^I have opened login page$")
     public void openLoginPage() {
-        NavigationService navigationService = new NavigationService();
         navigationService.navigateToLoginPage();
     }
 }
