d31ed71151494fc5b9c62170764c6b2f71cfe730
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2021-01-06 17:28:05 +0300

    add: add minor changes

--- src/test/java/awesome/shop/tests/cucumber/steps/SearchSteps.java
+++ src/test/java/awesome/shop/tests/cucumber/steps/SearchSteps.java
@@ -67,7 +67,7 @@ public class SearchSteps {
     }
 
     @Then("^\"(.*)\" product should be found$")
-    public void findSearchResultProduct(String expectedResultProduct) {
+    public void checkSearchResultProduct(String expectedResultProduct) {
         List<SearchResultFragment> searchResults = searchResultPage.getAllSearchResults();
         List<String> productNames = new ArrayList<>();
         for (SearchResultFragment searchResult : searchResults) {
@@ -78,7 +78,7 @@ public class SearchSteps {
     }
 
     @Then("^I should see incorrect search message \"(.*)\"$")
-    public void seeSearchMessage(String expectedResultMessage) {
+    public void verifySearchMessage(String expectedResultMessage) {
         Assert.assertEquals(searchResultPage.getIncorrectSearchCriteriaMessage(), expectedResultMessage,
                 expectedResultMessage + " isn't displayed");
     }
