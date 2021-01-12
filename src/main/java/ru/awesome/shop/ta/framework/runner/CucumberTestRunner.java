91a3dabce320c63ba3bf2917ffb788b1bb4d518d
Anastasiya Murashka <anastasiya_murashka@epam.com>
2021-01-12 17:50:14 +0300

    add grid with cucumber

--- src/main/java/ru/awesome/shop/ta/framework/runner/CucumberTestRunner.java
+++ src/main/java/ru/awesome/shop/ta/framework/runner/CucumberTestRunner.java
@@ -2,6 +2,7 @@ package ru.awesome.shop.ta.framework.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import ru.awesome.shop.ta.framework.listeners.SuiteListener;
 
@@ -14,4 +15,10 @@ import ru.awesome.shop.ta.framework.listeners.SuiteListener;
         features = "src/test/resources/features")
 @Listeners({SuiteListener.class})
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
