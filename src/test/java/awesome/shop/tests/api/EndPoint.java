d10b02892f9709a1de3ed36f27df8c4a69143756
Anastasiya Murashka <anastasiya_murashka@epam.com>
2021-01-25 09:04:58 +0300

    Create EndPoint.java

---
+++ src/test/java/awesome/shop/tests/api/EndPoint.java
@@ -0,0 +1,10 @@
+package awesome.shop.tests.api;
+
+
+public class EndPoint {
+    private final String BASE_URL = "https://awesome-shop.01sh.ru";
+
+    public String getBaseUrl() {
+        return BASE_URL;
+    }
+}
