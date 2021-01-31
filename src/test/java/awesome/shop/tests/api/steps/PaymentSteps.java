d036a887b964a737a55fa85f7a6c8f178ed2dd3d
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2021-02-01 00:06:13 +0300

    add: add minor changes

--- src/test/java/awesome/shop/tests/api/steps/PaymentSteps.java
+++ src/test/java/awesome/shop/tests/api/steps/PaymentSteps.java
@@ -28,20 +28,18 @@ public class PaymentSteps {
     @When("^I perform request to add payment address$")
     public void addPaymentAddress(DataTable dataTable) throws ParseException {
         Objects.requireNonNull(dataTable, "DataTable cannot be null");
-        HttpResponse<PaymentResponseBody> httpResponse;
         Map<String, String> form = dataTable.asMap(String.class, String.class);
         AddressRequestBody addressRequestBody = new AddressRequestBody
                 (form.get("First Name"), form.get("Last Name"), form.get("Address"),
                         form.get("City"), form.get("Country"), form.get("Zone_Id"));
-        httpResponse = paymentMicroservice.addPaymentAddress(addressRequestBody);
+        HttpResponse<PaymentResponseBody> httpResponse = paymentMicroservice.addPaymentAddress(addressRequestBody);
         apiTestContext.setActualStatusCode(httpResponse.getStatusCode());
         apiTestContext.setActualSuccessMessage(httpResponse.getBody().getSuccess());
     }
 
     @When("^I perform request to get payment method$")
     public void getPaymentMethod() {
-        HttpResponse<PaymentResponseBody> httpResponse;
-        httpResponse = paymentMicroservice.getPayments();
+        HttpResponse<PaymentResponseBody> httpResponse = paymentMicroservice.getPayments();
         apiTestContext.setActualStatusCode(httpResponse.getStatusCode());
         apiTestContext.setActualErrorMessage(httpResponse.getBody().getError());
     }
@@ -49,10 +47,9 @@ public class PaymentSteps {
     @When("^I perform request to set payment method$")
     public void setPaymentMethod(DataTable dataTable) throws ParseException {
         Objects.requireNonNull(dataTable, "DataTable cannot be null");
-        HttpResponse<PaymentResponseBody> httpResponse;
         Map<String, String> form = dataTable.asMap(String.class, String.class);
         PaymentRequestBody paymentRequestBody = new PaymentRequestBody(form.get("Payment Method"));
-        httpResponse = paymentMicroservice.setPayments(paymentRequestBody);
+        HttpResponse<PaymentResponseBody> httpResponse = paymentMicroservice.setPayments(paymentRequestBody);
         apiTestContext.setActualStatusCode(httpResponse.getStatusCode());
         apiTestContext.setActualSuccessMessage(httpResponse.getBody().getSuccess());
     }
